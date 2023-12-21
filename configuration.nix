a78d89f47cb10011a66cf51630269faaedbc7c02
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-12-21 03:00:11 +0300

    [configuration] add spacing

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,7 @@
     nixpkgs = {
         hostPlatform = lib.mkDefault "x86_64-linux";
         config.allowUnfree = true;
+
         config.packageOverrides = super: {
           python3-lto = super.python3.override {
             packageOverrides = python-self: python-super: {
