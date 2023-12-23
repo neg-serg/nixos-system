c4dba47e1e57eed3fed46eabde6801c260a2e2d2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-12-23 15:12:28 +0300

    [nvidia] remove modesetting

--- nvidia.nix
+++ nvidia.nix
@@ -60,7 +60,6 @@
     services.xserver={screenSection=''Option         "metamodes" "3440x1440_175 +0+0 {AllowGSYNCCompatible=Off}"'';};
     services.xserver.videoDrivers=["nvidia"];
     hardware.nvidia={
-        modesetting.enable=true; # Modesetting is required.
         open=false; # Currently alpha-quality/buggy, so false is currently the recommended setting.
         nvidiaSettings=true;
     };
