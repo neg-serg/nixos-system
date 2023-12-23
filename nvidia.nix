c42586bab0aea2e6b07b576cc064b1d643940def
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-12-23 14:02:59 +0300

    [nvidia] cleanup

--- nvidia.nix
+++ nvidia.nix
@@ -52,17 +52,15 @@
 
     environment={
         systemPackages=with pkgs; [glxinfo];
-        variables={ __GL_GSYNC_ALLOWED="0"; };
+        variables={ 
+            __GL_GSYNC_ALLOWED="0"; 
+        };
     };
 
     services.xserver={screenSection=''Option         "metamodes" "3440x1440_175 +0+0 {AllowGSYNCCompatible=Off}"'';};
     services.xserver.videoDrivers=["nvidia"];
     hardware.nvidia={
         modesetting.enable=true; # Modesetting is required.
-        # Nvidia power management. Experimental, and can cause sleep/suspend to fail. powerManagement.enable=false;
-        # Fine-grained power management. Turns off GPU when not in use.
-        # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-        powerManagement.finegrained=false;
         open=false; # Currently alpha-quality/buggy, so false is currently the recommended setting.
         nvidiaSettings=true;
     };
