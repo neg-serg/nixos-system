b8e37ea01e0fcf362538c3bf13eb0c39099aacd1
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-12-24 15:38:10 +0300

    [networkmanager] do not wait

--- networking.nix
+++ networking.nix
@@ -10,4 +10,8 @@
     networking.useDHCP = lib.mkDefault true;
     # networking.interfaces.enp6s0.useDHCP = lib.mkDefault true;
     # networking.interfaces.enp8s0.useDHCP = lib.mkDefault true;
+    systemd.services = {
+        # Unconditionally disable this Removes the time in the startup where the system waits for a connection
+        NetworkManager-wait-online.enable = false;
+    };
 }
