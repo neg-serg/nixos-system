0e2c1c61352449de088eb4a34e8914d439c897df
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-12-24 13:47:08 +0300

    [flake] remove

--- flake.nix
+++
@@ -1,9 +0,0 @@
-{
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.container = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [ ({ pkgs, ... }: { imports = [ ./configuration.nix ]; }) ];
-    };
-  };
-}
