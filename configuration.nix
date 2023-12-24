de7a16d0a23352634b1f52774f98bcbec8eeff1b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2023-12-24 15:14:56 +0300

    [steam] add

--- configuration.nix
+++ configuration.nix
@@ -151,6 +151,11 @@
         dconf = { enable = true; };
         mtr = { enable = true; };
         zsh = { enable = true; };
+        steam = {
+            enable = true;
+            remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+            dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+        };
     };
 
     services = {
