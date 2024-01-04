2878da98b426ec40cb31aaaf0eefb9f6b1abd0ad
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-01-05 02:25:00 +0300

    [flake] add nur and disko

--- flake.nix
+++ flake.nix
@@ -1,13 +1,15 @@
 {
     description = "Neg-Serg configuration";
     inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-        nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
-        nixpkgs-r2211.url = "github:NixOS/nixpkgs/nixos-22.11";
-        nix-gaming.url = "github:fufexan/nix-gaming";
-        nixtheplanet.url = "github:matthewcroughan/NixThePlanet";
         chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+        disko.url = "github:nix-community/disko";
         nh.url = "github:viperML/nh";
+        nix-gaming.url = "github:fufexan/nix-gaming";
+        nixpkgs-r2211.url = "github:NixOS/nixpkgs/nixos-22.11";
+        nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        nixtheplanet.url = "github:matthewcroughan/NixThePlanet";
+        nur.url = "github:nix-community/NUR";
     };
     outputs = {
         self
@@ -20,6 +22,8 @@
         , nixtheplanet
         , chaotic
         , nh
+        , nur
+        , disko
         } @inputs: {
         nixosConfigurations = {
             telfir = nixpkgs.lib.nixosSystem {
