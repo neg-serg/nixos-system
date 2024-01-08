4579cebdd0ee2df7c2f6d3d08a3f73c3c630ffef
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-01-09 00:15:24 +0300

    [networking] remove networkmanager

--- networking.nix
+++ networking.nix
@@ -2,13 +2,7 @@
 {
     networking.hostName = "telfir"; # Define your hostname.
     networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
-    networking.networkmanager.enable = true;
     networking.useDHCP = lib.mkDefault true;
-    systemd.services = {
-        NetworkManager-wait-online.enable = false; # Unconditionally disable this Removes the time in the startup where the system waits for a connection
-    };
-    # networking.networkmanager.dns = "systemd-resolved";
-    # services.resolved.enable = true;
     services.udev.extraRules = ''
         KERNEL=="eth*", ATTR{address}=="fc:34:97:b7:16:0e", NAME="net0"
         KERNEL=="eth*", ATTR{address}=="fc:34:97:b7:16:0f", NAME="net1"
