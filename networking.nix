c6133347cd8d23d330c901b612669d77983402a3
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-01-08 23:54:48 +0300

    [networking] cleanup

--- networking.nix
+++ networking.nix
@@ -3,25 +3,23 @@
     networking.hostName = "telfir"; # Define your hostname.
     networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
     networking.networkmanager.enable = true;
-    # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-    # (the default) this is the recommended approach. When using systemd-networkd it's
-    # still possible to use this option, but it's recommended to use it in conjunction
-    # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
     networking.useDHCP = lib.mkDefault true;
-    # networking.interfaces.enp6s0.useDHCP = lib.mkDefault true;
-    # networking.interfaces.enp8s0.useDHCP = lib.mkDefault true;
     systemd.services = {
-        # Unconditionally disable this Removes the time in the startup where the system waits for a connection
-        NetworkManager-wait-online.enable = false;
+        NetworkManager-wait-online.enable = false; # Unconditionally disable this Removes the time in the startup where the system waits for a connection
     };
-    # networking.search = [(builtins.readFile ./domains)];
-    networking.nameservers = [
-        "1.1.1.1"
-        "172.20.64.1" # OpenVPN defined name servers
-        "127.0.0.53" # System defined name servers
-    ];
+    # networking.networkmanager.dns = "systemd-resolved";
+    # services.resolved.enable = true;
     services.udev.extraRules = ''
         KERNEL=="eth*", ATTR{address}=="fc:34:97:b7:16:0e", NAME="net0"
         KERNEL=="eth*", ATTR{address}=="fc:34:97:b7:16:0f", NAME="net1"
     '';
+    environment.etc = {
+        "resolv.conf".text = ''
+            nameserver 1.1.1.1
+            nameserver 192.168.88.1
+            nameserver 172.20.64.1 # OpenVPN defined name servers
+            nameserver 127.0.0.53 # System defined name servers
+            options edns0
+        '';
+    };
 }
