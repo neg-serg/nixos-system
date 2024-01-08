bfe6c46b1f2e591e03b38bd958307b23c6b2b072
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-01-09 00:32:25 +0300

    [networking] CS

--- networking.nix
+++ networking.nix
@@ -1,28 +1,30 @@
 { config, lib, pkgs, modulesPath, ... }:
 {
-    networking.hostName = "telfir"; # Define your hostname.
-    networking.wireless.enable = false;  # Enables wireless support via wpa_supplicant.
-    systemd.network.enable = true;
-    systemd.network.networks."10-lan" = {
-        matchConfig.Name = "net0";
-        networkConfig.DHCP = "ipv4";
+    networking = {
+        hostName = "telfir"; # Define your hostname.
+        wireless.enable = false;  # Enables wireless support via wpa_supplicant.
+        useNetworkd = true;
+        nameservers = [
+            "1.1.1.1"
+            "192.168.88.1"
+            "172.20.64.1" # OpenVPN defined name servers
+            "127.0.0.53" # System defined name servers
+            # options edns0
+        ];
     };
-    systemd.network.networks."11-lan" = {
-        matchConfig.Name = "net1";
-        networkConfig.DHCP = "ipv4";
+    systemd.network = {
+        enable = true;
+        networks."10-lan" = {
+            matchConfig.Name = "net0";
+            networkConfig.DHCP = "ipv4";
+        };
+        networks."11-lan" = {
+            matchConfig.Name = "net1";
+            networkConfig.DHCP = "ipv4";
+        };
     };
-
     services.udev.extraRules = ''
         KERNEL=="eth*", ATTR{address}=="fc:34:97:b7:16:0e", NAME="net0"
         KERNEL=="eth*", ATTR{address}=="fc:34:97:b7:16:0f", NAME="net1"
     '';
-    environment.etc = {
-        "resolv.conf".text = ''
-            nameserver 1.1.1.1
-            nameserver 192.168.88.1
-            nameserver 172.20.64.1 # OpenVPN defined name servers
-            nameserver 127.0.0.53 # System defined name servers
-            options edns0
-        '';
-    };
 }
