36c433b5c563e1016109e4c1f68ef9e62f34a286
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-01-14 21:35:46 +0300

    [session] lightdm try again

--- session.nix
+++ session.nix
@@ -14,10 +14,33 @@
             autoLogin.enable = true;
             autoLogin.user = "neg";
             session = [{manage="desktop"; name="negwm"; start=''$HOME/.xsession'';}];
-            gdm = {
-                enable = true;
-                wayland = true;
-                autoSuspend = false;
+            lightdm = {
+                greeters.mini = {
+                    enable = true;
+                    user = "neg";
+                    extraConfig = ''
+                        [greeter]
+                        show-password-label = true
+                        password-label-text = Welcome home, great slayer
+                        invalid-password-text = Are you sure?
+                        show-input-cursor = false
+                        password-alignment = left
+
+                        [greeter-theme]
+                        font = "Iosevka Regular"
+                        font-size = 14pt
+                        text-color = "#666666"
+                        error-color = "#FF0000"
+                        background-image = "/home/neg/pic/wl/wallhaven-wewqop.jpg"
+                        background-color = "#000000"
+                        window-color = "#000000"
+                        border-color = "#333333"
+                        border-width = 1px
+                        layout-space = 14
+                        password-color = "#666666"
+                        password-background-color = "#222222"
+                        '';
+                };
             };
         };
     };
