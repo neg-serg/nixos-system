8ca73e7834c0e6b656166bb64ba66492d6d30b6c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-01-14 18:48:20 +0300

    [configuration] CS

--- configuration.nix
+++ configuration.nix
@@ -50,11 +50,11 @@
     # This is using a rec (recursive) expression to set and access XDG_BIN_HOME within the expression
     # For more on rec expressions see https://nix.dev/tutorials/first-steps/nix-language#recursive-attribute-set-rec
     environment.sessionVariables = rec {
+        XDG_BIN_HOME = "$HOME/.local/bin";
         XDG_CACHE_HOME = "$HOME/.cache";
         XDG_CONFIG_HOME = "$HOME/.config";
         XDG_DATA_HOME = "$HOME/.local/share";
         XDG_STATE_HOME = "$HOME/.local/state";
-        XDG_BIN_HOME = "$HOME/.local/bin";
 
         XDG_DESKTOP_DIR="$HOME/1st_level/desktop";
         XDG_DOCUMENTS_DIR="$HOME/doc";
