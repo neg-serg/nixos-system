8f53ef2beb8b189e49c2aa80475e95b610b3a1c5
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-01-14 23:46:41 +0300

    [sddm] usermode

--- session.nix
+++ session.nix
@@ -1,8 +1,6 @@
 { config, pkgs, ... }:
-let 
-     tokyo-night-sddm = pkgs.libsForQt5.callPackage ./tokyo-night-sddm/default.nix { };
-in {
-    environment.systemPackages = with pkgs; [ tokyo-night-sddm ];
+let tokyo-night-sddm = pkgs.libsForQt5.callPackage ./tokyo-night-sddm/default.nix { }; in {
+    environment.systemPackages = with pkgs; [tokyo-night-sddm];
     services.xserver = {
         enable = true; # Enable the X11 windowing system.
         exportConfiguration = true;
@@ -17,7 +15,16 @@ in {
             autoLogin.enable = false;
             autoLogin.user = "neg";
             session = [{manage="desktop"; name="negwm"; start=''$HOME/.xsession'';}];
-            sddm = { enable = true; theme = "tokyo-night-sddm"; };
+            sddm = {
+                enable = true;
+                theme = "tokyo-night-sddm";
+                wayland.enable = true;
+                settings = {
+                    General = {
+                        DisplayServer = "x11-user";
+                    };
+                };
+            };
         };
     };
 }
