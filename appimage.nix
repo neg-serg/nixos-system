1c0abb4aa77c4b6a0d173afa8ad5b3da167aa728
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-01-20 02:26:39 +0300

    [appimage] add support example

---
+++ appimage.nix
@@ -0,0 +1,11 @@
+{ config, lib, pkgs, modulesPath, packageOverrides, ... }: {
+    # run appimages directly (see https://nixos.wiki/wiki/Appimage)
+    boot.binfmt.registrations.appimage = {
+        wrapInterpreterInShell = false;
+        interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+        recognitionType = "magic";
+        offset = 0;
+        mask = "\\xff\\xff\\xff\\xff\\x00\\x00\\x00\\x00\\xff\\xff\\xff";
+        magicOrExtension = "\\x7fELF....AI\\x02";
+    };
+}
