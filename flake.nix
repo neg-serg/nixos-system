35ca9c16a39b17fdcfd0f6fc1adf8e855d33c5da
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-01-20 03:27:31 +0300

    [flake] cleanup agenix

--- flake.nix
+++ flake.nix
@@ -32,7 +32,6 @@
                     ./configuration.nix
                     chaotic.nixosModules.default
                     nix-gaming.nixosModules.pipewireLowLatency
-                    agenix.nixosModules.default { environment.systemPackages = [ agenix.packages."x86_64-linux".default ]; }
                 ];
             };
         };
