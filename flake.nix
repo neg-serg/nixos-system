14fa52e3f13b10b602f261a992cd9026c700c0b7
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-01-20 03:26:59 +0300

    [flake] sort io

--- flake.nix
+++ flake.nix
@@ -5,25 +5,25 @@
         disko.url = "github:nix-community/disko";
         nh.url = "github:viperML/nh";
         nix-gaming.url = "github:fufexan/nix-gaming";
+        nix-ld.url = "github:Mic92/nix-ld";
         nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
         nixtheplanet.url = "github:matthewcroughan/NixThePlanet";
         nur.url = "github:nix-community/NUR";
-        nix-ld.url = "github:Mic92/nix-ld";
     };
     outputs = {
         self
+        , chaotic
+        , disko
+        , nh
         , nix
+        , nix-gaming
+        , nix-ld
         , nixos-hardware
         , nixpkgs
         , nixpkgs-stable
-        , nix-gaming
         , nixtheplanet
-        , chaotic
-        , nh
         , nur
-        , disko
-        , nix-ld
         } @inputs: {
         nixosConfigurations = {
             telfir = nixpkgs.lib.nixosSystem {
