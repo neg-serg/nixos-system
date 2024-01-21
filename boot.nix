2367e56fbde82467c1e51417c945505d61913eed
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-01-21 17:12:42 +0300

    [boot] refactoring

--- boot.nix
+++ boot.nix
@@ -1,25 +1,26 @@
-{ config, lib, pkgs, modulesPath, packageOverrides, ... }:
-{
-    boot.loader = {
-        efi.canTouchEfiVariables = true;
-        systemd-boot.consoleMode = "max";
-        systemd-boot.editor = false; # close security hole
-        systemd-boot.enable = true;
-        timeout = 3;
-    };
-    boot.initrd = {
-        availableKernelModules = [
-            "nvidia"
-            "nvidia_drm"
-            "nvidia_modeset"
-            "nvidia_uvm"
-            "nvme"
-            "sd_mod"
-            "usb_storage"
-            "usbhid"
-            "xhci_hcd"
-            "xhci_pci"
-        ];
-        kernelModules = ["dm-snapshot"];
+{ config, lib, pkgs, modulesPath, packageOverrides, ... }: {
+    boot = {
+        loader = {
+            efi.canTouchEfiVariables = true;
+            systemd-boot.consoleMode = "max";
+            systemd-boot.editor = false; # close security hole
+                systemd-boot.enable = true;
+            timeout = 3;
+        };
+        initrd = {
+            availableKernelModules = [
+                "nvidia"
+                "nvidia_drm"
+                "nvidia_modeset"
+                "nvidia_uvm"
+                "nvme"
+                "sd_mod"
+                "usb_storage"
+                "usbhid"
+                "xhci_hcd"
+                "xhci_pci"
+            ];
+            kernelModules = ["dm-snapshot"];
+        };
     };
 }
