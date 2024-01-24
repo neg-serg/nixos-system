8bb1c01d9c3e1c5447954dc74b0540af951d3197
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-01-24 14:11:47 +0300

    [bluetooth] try tws support

--- configuration.nix
+++ configuration.nix
@@ -141,6 +141,18 @@
         lowLatency = { enable = true; quantum = 64; rate = 48000; };
     };
 
+    # bluetooth support(maybe not needed, check it later)
+    environment.etc = {
+        "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+            bluez_monitor.properties = {
+                ["bluez5.enable-sbc-xq"] = true,
+                ["bluez5.enable-msbc"] = true,
+                ["bluez5.enable-hw-volume"] = true,
+                ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+            }
+        '';
+    };
+
     users = {
         users.neg = {
             packages = with pkgs; [pam_u2f python3-lto];
