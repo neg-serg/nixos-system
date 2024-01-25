45007dd63e978e1cec521e8096b7d94d159d861e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-01-25 23:42:11 +0300

    [example] CS

--- nix-ld-example.nix
+++ nix-ld-example.nix
@@ -1,20 +1,20 @@
 nix-ld = {
-            enable = true;
-            libraries = with pkgs; [
-                stdenv.cc.cc # commonly needed
-                zlib # commonly needed
-                openssl # commonly needed
-                mesa
-                glib
-                fontconfig
-                libgpg-error
-                freetype
-                alsa-lib
-                dbus
-                expat
-                libz
-                nss
-                pciutils
-                util-linux
-            ];
-        };
+    enable = true;
+    libraries = with pkgs; [
+        stdenv.cc.cc # commonly needed
+            zlib # commonly needed
+            openssl # commonly needed
+            mesa
+            glib
+            fontconfig
+            libgpg-error
+            freetype
+            alsa-lib
+            dbus
+            expat
+            libz
+            nss
+            pciutils
+            util-linux
+    ];
+};
