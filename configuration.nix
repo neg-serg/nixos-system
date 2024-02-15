73b10209d574c3941c4d613dd6e6d779c6e5597d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-16 02:22:47 +0300

    [gamescope] add

--- configuration.nix
+++ configuration.nix
@@ -203,8 +203,9 @@
         };
         steam = {
             enable = true;
-            remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
             dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+            gamescopeSession.enable = true;
+            remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
         };
     };
 
