d940a689772c5d98aa26018b787ca9a28194757b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-17 03:30:11 +0300

    [kmscon] add iosevka

--- kmscon.nix
+++ kmscon.nix
@@ -1,6 +1,7 @@
-{ config, lib, pkgs, modulesPath, packageOverrides, ... }: {
+{ config, lib, pkgs, ... }: {
+    environment.systemPackages = with pkgs; [ iosevka ];
     services.kmscon = {
-        enable = false;
+        enable = true;
         hwRender = true;
         extraOptions = "--term xterm-256color --font-size 10 --font-name Iosevka";
     };
