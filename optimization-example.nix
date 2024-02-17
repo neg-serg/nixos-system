7745994255af2e145e9b2839ddd7fbb611d77f49
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-02-17 21:42:55 +0300

    [configuration] cleanup

--- optimization-example.nix
+++ optimization-example.nix
@@ -1,6 +1,6 @@
-nix.settings.system-features = [ "big-parallel" "kvm" "gccarch-znver3" "gcctune-znver3" ];
-nixpkgs.hostPlatform = {
-    gcc.arch = "znver3";
-    gcc.tune = "znver3";
-    system = "x86_64-linux";
-};
+# nix.settings.system-features = [ "big-parallel" "kvm" "gccarch-znver3" "gcctune-znver3" ];
+# nixpkgs.hostPlatform = {
+#     gcc.arch = "znver3";
+#     gcc.tune = "znver3";
+#     system = "x86_64-linux";
+# };
