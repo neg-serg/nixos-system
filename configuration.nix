c68bba56354a69ba63eaa1fb7014c5e1b7f39447
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-01 01:12:13 +0300

    [wireplumber] comment for now

--- configuration.nix
+++ configuration.nix
@@ -144,20 +144,20 @@
         alsa.support32Bit = true;
         pulse.enable = true;
         jack.enable = true;
-        lowLatency = { enable = true; quantum = 128; rate = 48000; };
+        # lowLatency = { enable = true; quantum = 128; rate = 48000; };
     };
 
-    # bluetooth support(maybe not needed, check it later)
-    environment.etc = {
-        "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-            bluez_monitor.properties = {
-                ["bluez5.enable-sbc-xq"] = true,
-                ["bluez5.enable-msbc"] = true,
-                ["bluez5.enable-hw-volume"] = true,
-                ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-            }
-        '';
-    };
+    # # bluetooth support(maybe not needed, check it later)
+    # environment.etc = {
+    #     "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+    #         bluez_monitor.properties = {
+    #             ["bluez5.enable-sbc-xq"] = true,
+    #             ["bluez5.enable-msbc"] = true,
+    #             ["bluez5.enable-hw-volume"] = true,
+    #             ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+    #         }
+    #     '';
+    # };
 
     users = {
         users.neg = {
