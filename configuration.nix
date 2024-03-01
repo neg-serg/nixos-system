87861a90fedab9ae49092e80e5644933860866c7
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-01 14:25:01 +0300

    [steam] try fix gamescope

--- configuration.nix
+++ configuration.nix
@@ -158,6 +158,12 @@
     #     '';
     # };
 
+    nixpkgs.config.packageOverrides = pkgs: {
+      steam = pkgs.steam.override {
+               extraPkgs = pkgs: with pkgs; [ xorg.libXcursor xorg.libXi xorg.libXinerama xorg.libXScrnSaver libpng libpulseaudio libvorbis stdenv.cc.cc.lib libkrb5 keyutils ];
+      };
+    };
+
     users = {
         users.neg = {
             isNormalUser = true;
