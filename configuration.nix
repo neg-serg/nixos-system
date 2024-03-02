c2193814d9fbe9a49fc0d358c46096d3036e2a90
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-02 06:13:20 +0300

    [pipewire] export

--- configuration.nix
+++ configuration.nix
@@ -17,6 +17,7 @@
         ./session.nix
         ./udev-rules.nix
         ./vnstat.nix
+        ./pipewire.nix
     ];
     nix = {
         settings = {
@@ -139,33 +140,6 @@
     hardware.enableAllFirmware = true; # Enable all the firmware
     hardware.enableRedistributableFirmware = true;
     hardware.openrazer.enable = true; # Enable the OpenRazer driver for my Razer stuff
-    services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-        jack.enable = true;
-        # Once #292115 is merged and has reached nixos-unstable, you'll be able to use services.pipewire.wireplumber.extraLuaConfig as well:
-        # services.pipewire.wireplumber.extraLuaConfig.bluetooth."51-bluez-config" = ''
-        # 	bluez_monitor.properties = {
-        # 		["bluez5.enable-sbc-xq"] = true,
-        # 		["bluez5.enable-msbc"] = true,
-        # 		["bluez5.enable-hw-volume"] = true,
-        # 		["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-        # 	}
-        # '';
-        wireplumber.configPackages = [
-            # bluetooth support(maybe not needed, check it later)
-            (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
-                bluez_monitor.properties = {
-                    ["bluez5.enable-sbc-xq"] = true,
-                    ["bluez5.enable-msbc"] = true,
-                    ["bluez5.enable-hw-volume"] = true,
-                    ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-                }
-            '')
-        ];
-    };
 
     nixpkgs.config.packageOverrides = pkgs: {
       steam = pkgs.steam.override {
