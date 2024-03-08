da9a1cf4dd12ce70076d16645353358bc5277b94
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-08 16:17:15 +0300

    [amd] add

---
+++ amd.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }: {
+    hardware.opengl={
+        enable=true;
+        driSupport=true;
+        driSupport32Bit=true;
+
+        extraPackages = with pkgs; [ amdvlk ];
+        # For 32 bit applications 
+        extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
+    };
+
+    environment={
+        systemPackages=with pkgs; [
+            glxinfo
+            vulkan-extension-layer
+            vulkan-tools
+            vulkan-validation-layers
+        ];
+    };
+    services.xserver={
+        enable=true;
+        screenSection=''Option "metamodes" "3440x1440_175 +0+0"'';
+        monitorSection=''
+            Option "StandbyTime" "0"
+            Option "SuspendTime" "0"
+            Option "OffTime" "0"
+            Option "BlankTime" "0"
+        '';
+    };
+    services.xserver.videoDrivers=["amdgpu"];
+}
