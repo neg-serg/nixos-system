96ebe432f85ad618a14cb29c5c665f33341bac64
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-09 00:21:50 +0300

    [amd] add vrr support, try set resolution

--- amd.nix
+++ amd.nix
@@ -14,11 +14,15 @@
     };
     services.xserver={
         enable=true;
+        screenSection=''Option "metamodes" "3440x1440_175 +0+0"'';
         monitorSection=''
             Option "StandbyTime" "0"
             Option "SuspendTime" "0"
             Option "OffTime" "0"
             Option "BlankTime" "0"
+        '';
+        deviceSection=''
+            Option "VariableRefresh" "true"
         '';
     };
     services.xserver.videoDrivers=["amdgpu"];
