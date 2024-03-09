1297b9a8e8436e8a0ac0e857eb7cd1fdd8691277
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-09 04:46:26 +0300

    [nix] add programs to pin dependecies

--- pkgs.nix
+++ pkgs.nix
@@ -79,12 +79,14 @@
         deadnix # scan for dead nix code
         manix # nixos documentation
         nh # some nice nix commands
+        niv # pin different stuff
         nix-diff # show what causes derivation to be different
         nix-index # index for nix-locate
         nix-init # provides more easy way to create nix packages
         nix-output-monitor # fancy nix output (nom)
         nix-tree # Interactive scan current system / derivations for what-why-how depends
         nixos-shell # tool to create vm for current config
+        npins # pin different stuff ( inspired by niv )
         nurl # cli to generate Nix fetcher calls from repository URLs
         nvd # compare versions: nvd diff /run/current-system result
         statix # static analyzer for nix
