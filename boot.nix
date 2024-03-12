ad3f38c67345ab1cf6fcfde66a641e0139dc0203
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-12 11:37:43 +0300

    [grub] try

--- boot.nix
+++ boot.nix
@@ -2,11 +2,16 @@
     boot = {
         loader = {
             efi.canTouchEfiVariables = true;
-            systemd-boot = {
-                consoleMode = "max";
-                editor = false; # close security hole
-                enable = true;
-            };
+            # systemd-boot = {
+            #     consoleMode = "max";
+            #     editor = false; # close security hole
+            #     enable = true;
+            # };
+            grub.enable = true;
+            grub.version = 2;
+            grub.efiSupport = true;
+            grub.useOSProber = true;
+            grub.device = "nodev";
             timeout = 1;
         };
         kernelPackages = pkgs.linuxPackages_cachyos;
