30b5daa32f99a940b401e150720349e17a097536
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-17 15:50:11 +0300

    [trash] cleanup

--- trash/pkgs.nix
+++ trash/pkgs.nix
@@ -1,8 +1,7 @@
-{ config, lib, pkgs, modulesPath, packageOverrides, inputs, ... }: {
+{ pkgs, ... }: {
     environment.systemPackages = with pkgs; [
         # hddtemp # display hard disk temperature
         # hdparm # set ata/sata params
-        # pam_u2f  # A PAM module for allowing authentication with a U2F device
         # flatpak-builder # build flatpaks
         # hashcat # password recovery
         # qFlipper # desktop stuff for flipper zero
