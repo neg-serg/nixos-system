b292f6ad525bcac4bb9261e194a91156c017fae3
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-17 19:18:21 +0300

    [pipewire] cleanup

--- pipewire.nix
+++ pipewire.nix
@@ -4,18 +4,8 @@
         alsa.enable = true;
         alsa.support32Bit = true;
         pulse.enable = true;
-        jack.enable = true;
-        # Once #292115 is merged and has reached nixos-unstable, you'll be able to use services.pipewire.wireplumber.extraLuaConfig as well:
-        # services.pipewire.wireplumber.extraLuaConfig.bluetooth."51-bluez-config" = ''
-        # 	bluez_monitor.properties = {
-        # 		["bluez5.enable-sbc-xq"] = true,
-        # 		["bluez5.enable-msbc"] = true,
-        # 		["bluez5.enable-hw-volume"] = true,
-        # 		["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-        # 	}
-        # '';
+        jack.enable = false;
         wireplumber.configPackages = [
-            # bluetooth support(maybe not needed, check it later)
             (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
                 bluez_monitor.properties = {
                     ["bluez5.enable-sbc-xq"] = true,
