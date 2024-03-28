b09318912d352bd7046759ce58b358ff401f71d9
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-29 02:55:29 +0300

    [amd] minor fixes

--- amd.nix
+++ amd.nix
@@ -17,7 +17,7 @@
         xrandrHeads = [{
             output = "DisplayPort-1";
             monitorConfig = ''
-                # Find these values in "/var/log/Xorg.0.log. They will be in this exact format so scroll until you find it.
+                # Find these values in "$HOME/.local/share/xorg/Xorg.0.log They will be in this exact format so scroll until you find it.
                 Modeline    "3440x1440_175"  1019.75  3440 3488 3520 3600  1440 1443 1453 1619 +hsync -vsync
                 Option		"PreferredMode"	"3440x1440_175"
                 '';
@@ -31,7 +31,6 @@
         deviceSection=''
             Option "VariableRefresh" "false"
             Option "EnablePageFlip" "off"
-            #Option "AsyncFlipSecondaries" "false"
         '';
     };
     services.xserver.videoDrivers=["amdgpu"];
