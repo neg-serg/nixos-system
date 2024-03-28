37fa945aefa84ea0313217dcac96520f716b58d1
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-29 02:01:25 +0300

    [amd] try fix mode

--- amd.nix
+++ amd.nix
@@ -14,11 +14,11 @@
     };
     services.xserver={
         enable=true;
-        #screenSection=''Option "metamodes" "3440x1440_175 +0+0"'';
+        screenSection=''Option "metamodes" "3440x1440_175 +0+0"'';
         monitorSection=''
-            #Identifier	"DisplayPort-0" # Not sure if this needs to be the same as in "xrandr -q".
-            #Modeline   "3440x1440_175"x0.0  349.25  3440 3488 3520 3600  1440 1443 1453 1618 +hsync -vsync (97.0 kHz eP) # Find these values in "/var/log/Xorg.0.log. They will be in this exact format so scroll until you find it.
-            #Option		"PreferredMode"	"3440x1440_175"
+            # Find these values in "/var/log/Xorg.0.log. They will be in this exact format so scroll until you find it.
+            #Modeline    ""3440x1440"x175.0  1019.75  3440 3488 3520 3600  1440 1443 1453 1619 +hsync -vsync"
+            Option		"PreferredMode"	"3440x1440_175.0"
             Option      "StandbyTime" "0"
             Option      "SuspendTime" "0"
             Option      "OffTime" "0"
@@ -26,6 +26,8 @@
         '';
         deviceSection=''
             Option "VariableRefresh" "false"
+            #Option "AsyncFlipSecondaries" "false"
+            #Option "EnablePageFlip" "off"
         '';
     };
     services.xserver.videoDrivers=["amdgpu"];
