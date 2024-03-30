fc43dad1090622b9338cd8732999febc1d6bd879
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-30 03:13:58 +0300

    [syncthing] fix

--- syncthing.nix
+++ syncthing.nix
@@ -8,9 +8,16 @@
        overrideFolders = true;     # overrides any folders added or deleted through the WebUI
        settings = {
          devices = {
+           "telfir" = { id = "GNB553I-2IUWVBA-OCGXUVZ-BP322Q2-S5EGA5O-HNI5IWM-TJ4CEI6-547A5QI"; };
            "Pixel 7 Pro" = { id = "OWGOTRT-Q4LV2MR-QLVIFZH-LPWZ4DP-TANYCAM-SXC2W2A-BL4VSHS-KWXLVAB"; };
          };
-       };
+         folders = {
+           "music-upload" = { # Name of folder in Syncthing, also the folder ID
+             path = "/zero/music-upload"; # Which folder to add to Syncthing
+             devices = [ "Pixel 7 Pro" "telfir" ]; # Which devices to share the folder with
+           };
+        };
+      };
    };
    # Syncthing ports: 8384 for remote access to GUI
    # 22000 TCP and/or UDP for sync traffic
