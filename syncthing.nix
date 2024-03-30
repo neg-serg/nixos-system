4b71a6947c378f380e03107ae2934dfb57623aee
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-30 03:53:43 +0300

    [syncthing] cleanup again

--- syncthing.nix
+++ syncthing.nix
@@ -12,17 +12,16 @@
        overrideFolders = true; # overrides any folders added or deleted through the WebUI
        settings = {
          devices = {
-           "telfir" = { id = "GNB553I-2IUWVBA-OCGXUVZ-BP322Q2-S5EGA5O-HNI5IWM-TJ4CEI6-547A5QI"; };
            "Pixel 7 Pro" = { id = "OWGOTRT-Q4LV2MR-QLVIFZH-LPWZ4DP-TANYCAM-SXC2W2A-BL4VSHS-KWXLVAB"; };
          };
          folders = {
            "music-upload" = { # Name of folder in Syncthing, also the folder ID
                path = "/zero/syncthing/music-upload"; # Which folder to add to Syncthing
-               devices = [ "Pixel 7 Pro" "telfir" ]; # Which devices to share the folder with
+               devices = [ "Pixel 7 Pro" ]; # Which devices to share the folder with
            };
            "picture-upload" = { # Name of folder in Syncthing, also the folder ID
                path = "/zero/syncthing/picture-upload"; # Which folder to add to Syncthing
-               devices = [ "Pixel 7 Pro" "telfir" ]; # Which devices to share the folder with
+               devices = [ "Pixel 7 Pro" ]; # Which devices to share the folder with
            };
         };
       };
