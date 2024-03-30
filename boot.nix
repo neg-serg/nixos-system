6d98d025b051ea145939c5e370b39d23f3e64b07
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-03-31 02:11:53 +0300

    [grub] try new theme

--- boot.nix
+++ boot.nix
@@ -1,4 +1,12 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+with {
+  fallout = pkgs.fetchFromGitHub {
+    owner = "shvchk";
+    repo = "fallout-grub-theme";
+    rev = "80734103d0b48d724f0928e8082b6755bd3b2078";
+    sha256 = "sha256-7kvLfD6Nz4cEMrmCA9yq4enyqVyqiTkVZV5y4RyUatU=";
+  };
+}; {
     boot = {
         loader = {
             efi.canTouchEfiVariables = true;
@@ -12,6 +20,15 @@
                 fontSize = 32;
                 gfxmodeEfi = "2560x1440";
                 gfxpayloadEfi = "keep";
+                theme = fallout;
+                # theme = "${
+                #     (pkgs.fetchFromGitHub {
+                #      owner = "13atm01";
+                #      repo = "GRUB-Theme";
+                #      rev = "95bcc240162bce388ac2c0bec628b2aaa56e6cb8";
+                #      sha256 = "0xnx82fdyjqw89qmacwmlva9lis3zs8b0l1xi67njpypjy29sdnc";
+                #      })
+                # }/Touhou\ Project/Touhou-project/";
             };
             timeout = 1;
         };
