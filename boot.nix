9a864b9155d775c82e8e3cf46f61777882c9bf07
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-03 21:00:54 +0000

    [boot] cleanup fallout theme

--- boot.nix
+++ boot.nix
@@ -1,12 +1,4 @@
-{ pkgs, ... }:
-with {
-  fallout = pkgs.fetchFromGitHub {
-    owner = "shvchk";
-    repo = "fallout-grub-theme";
-    rev = "80734103d0b48d724f0928e8082b6755bd3b2078";
-    sha256 = "sha256-7kvLfD6Nz4cEMrmCA9yq4enyqVyqiTkVZV5y4RyUatU=";
-  };
-}; {
+{ pkgs, ... }: {
     boot = {
         loader = {
             efi.canTouchEfiVariables = true;
