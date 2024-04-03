6888ce4c7fc7bd0f5dbb21620ec96f4d6db3ebf0
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-03 03:19:04 +0300

    [pipewire] try fix crackling, again

--- pipewire.nix
+++ pipewire.nix
@@ -13,6 +13,45 @@
                     ["bluez5.enable-hw-volume"] = true,
                     ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
                 }
+                monitor.alsa.rules = [
+                  {
+                    matches = [
+                      {
+                        # Matches all sources
+                        node.name = "~alsa_input.*"
+                      },
+                      {
+                        # Matches all sinks
+                        node.name = "~alsa_output.*"
+                      }
+                    ]
+                    actions = {
+                      update-props = {
+                        session.suspend-timeout-seconds = 0
+                      }
+                    }
+                  }
+                ]
+                # bluetooth devices
+                monitor.bluez.rules = [
+                  {
+                    matches = [
+                      {
+                        # Matches all sources
+                        node.name = "~bluez_input.*"
+                      },
+                      {
+                        # Matches all sinks
+                        node.name = "~bluez_output.*"
+                      }
+                    ]
+                    actions = {
+                      update-props = {
+                        session.suspend-timeout-seconds = 0
+                      }
+                    }
+                  }
+                ]
             '')
         ];
     };
