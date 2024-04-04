aba6ece850f40e71112bbd826168316465101552
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-04 17:01:47 +0300

    [udev] cpu dma access from user

--- udev-rules.nix
+++ udev-rules.nix
@@ -3,5 +3,6 @@
         # High performance clocks for audio
         KERNEL=="rtc0", GROUP="audio"
         KERNEL=="hpet", GROUP="audio"
+        DEVPATH=="/devices/virtual/misc/cpu_dma_latency", OWNER="root", GROUP="audio", MODE="0660"
     '';
 }
