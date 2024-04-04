64ac6945300ef8908cfd3d98aa1e9a465bd3d2f5
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-04 23:41:10 +0300

    [kernel] disable intel hda for now

--- kernel.nix
+++ kernel.nix
@@ -12,12 +12,12 @@ let
       "systemd.show_status=false"
       "vt.global_cursor_default=0"
   ];
-  # intel_hda_modules = [
-  #       "snd_hda_codec"
-  #       "snd_hda_codec_hdmi"
-  #       "snd_hda_core"
-  #       "snd_hda_intel"
-  # ];
+  intel_hda_modules = [
+        "snd_hda_codec"
+        "snd_hda_codec_hdmi"
+        "snd_hda_core"
+        "snd_hda_intel"
+  ];
   # extra_security = [
   #     "page_poison=1" # Overwrite free'd memory
   #     "page_alloc.shuffle=1" # Enable page allocator randomization
@@ -103,7 +103,7 @@ let
     boot.kernelModules = ["kvm-amd" "tcp_bbr"];
     boot.blacklistedKernelModules=[
         "sp5100_tco" # Disable watchdog for better performance wiki.archlinux.org/title/improving_performance#Watchdogs
-    ] ++ obscure_network_protocols ++ old_rare_insufficiently_audited_fs;
+    ] ++ obscure_network_protocols ++ old_rare_insufficiently_audited_fs ++ intel_hda_modules;
     boot.kernelParams = f2fs_root_settings ++ [
         "rootflags=rw,relatime,lazytime,background_gc=on,discard,no_heap,user_xattr,inline_xattr,acl,inline_data,inline_dentry,flush_merge,extent_cache,mode=adaptive,active_logs=6,alloc_mode=default,fsync_mode=posix"
         "audit=0"
