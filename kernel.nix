490c2e65fbb07f662d705ee84866399882f7acca
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-06 01:08:47 +0300

    [kernel] cleanup

--- kernel.nix
+++ kernel.nix
@@ -57,7 +57,7 @@ let
       "ufs"
       "vivid"
       ];
-    in { pkgs, ... } : {
+    in { ... } : {
     # thx to https://github.com/hlissner/dotfiles
     boot.kernel.sysctl = {
         # The Magic SysRq key is a key combo that allows users connected to the
