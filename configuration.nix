5fda7c35540910d8d106ff5256894a64976b6993
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-05 10:44:37 +0300

    [musnix] try again

--- configuration.nix
+++ configuration.nix
@@ -25,8 +25,9 @@
     ];
     musnix.enable = true;
     musnix.kernel.realtime = true;
-    musnix.kernel.packages = pkgs.linuxPackages_6_6_rt;
+    musnix.kernel.packages = pkgs.linuxPackages-rt_latest;
     musnix.rtirq.enable = true;
+    musnxi.rtcqs.enable = true;
     musnix.das_watchdog.enable = true;
     nix = {
         settings = {
