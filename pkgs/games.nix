aaeeea4a52adf5ae874223e190f5dabe7a6e4521
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-13 01:47:56 +0300

    [games] refactoring

--- pkgs/games.nix
+++ pkgs/games.nix
@@ -1,6 +1,11 @@
 { pkgs, inputs, ... }: {
+    boot.kernel.sysctl = {
+        # NixOS configuration for Star Citizen requirements
+        "vm.max_map_count" = 16777216;
+        "fs.file-max" = 524288;
+    };
     environment.systemPackages = with pkgs; [
-        (inputs.nix-gaming.packages.${pkgs.hostPlatform.system}.star-citizen.override {
+        (inputs.nix-gaming.packages.${hostPlatform.system}.star-citizen.override {
             location="$HOME/games/star-citizen";
         })
     ];
