f5a1b2dae772adf8367b0f0d729aa7c0e2c56a09
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-15 02:13:12 +0300

    [udev] set none scheduler

--- udev-rules.nix
+++ udev-rules.nix
@@ -4,5 +4,6 @@
         KERNEL=="rtc0", GROUP="audio"
         KERNEL=="hpet", GROUP="audio"
         DEVPATH=="/devices/virtual/misc/cpu_dma_latency", OWNER="root", GROUP="audio", MODE="0660"
+        ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="none"
     '';
 }
