00027336df26631b53197c2cbd18b424d06ac93e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-17 01:46:49 +0300

    [configuration] cleanup

--- configuration.nix
+++ configuration.nix
@@ -2,9 +2,8 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 {
     imports = [ ./system ./hardware ./nix ./user ./pkgs ];
-    # (man configuration.nix or on https://nixos.org/nixos/options.html).
     system = {
-        stateVersion = "23.11"; # Did you read the comment?
+        stateVersion = "23.11"; # (man configuration.nix or on https://nixos.org/nixos/options.html).
         autoUpgrade.enable = true;
         autoUpgrade.allowReboot = true;
     };
