54cdc254db1e0206aed4ff897b88a2045588f221
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-17 01:37:03 +0300

    [security] move pkcs11 stuff here

--- configuration.nix
+++ configuration.nix
@@ -5,12 +5,6 @@
 
     virtualisation.docker.enable = true;
 
-    # Tell p11-kit to load/proxy opensc-pkcs11.so, providing all available slots
-    # (PIN1 for authentication/decryption, PIN2 for signing).
-    environment.etc."pkcs11/modules/opensc-pkcs11".text = ''
-        module: ${pkgs.opensc}/lib/opensc-pkcs11.so
-    '';
-
     hardware.i2c.enable = true;
     hardware.bluetooth = {
         enable = true;
--- system/security.nix
+++ system/security.nix
@@ -1,4 +1,10 @@
 { pkgs, ... }: {
+    # Tell p11-kit to load/proxy opensc-pkcs11.so, providing all available slots
+    # (PIN1 for authentication/decryption, PIN2 for signing).
+    environment.etc."pkcs11/modules/opensc-pkcs11".text = ''
+        module: ${pkgs.opensc}/lib/opensc-pkcs11.so
+    '';
+    
     security = {
         pam = {
             loginLimits = [
