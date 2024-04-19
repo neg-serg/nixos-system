d94aac6d8493f5d901a8ff2df608f1a654430f46
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-20 00:53:01 +0300

    [kde] try

--- user/session.nix
+++ user/session.nix
@@ -2,6 +2,7 @@
     environment.systemPackages = with pkgs; [
         xorg.xdpyinfo # display info
     ];
+    services.desktopManager.plasma6.enable = true;
     services.xserver = {
         enable = true; # Enable the X11 windowing system.
         exportConfiguration = true;
@@ -14,16 +15,19 @@
         };
         libinput.enable = true; # Enable touchpad support (enabled default in most desktopManager).
         desktopManager = { xterm.enable = false; };
-        displayManager = {
-            startx.enable = true;
-            session = [{ manage="window"; name="i3"; start=''$HOME/.xsession''; }];
-        };
+        displayManager.defaultSession = "plasma";
+        displayManager.sddm.wayland.enable = true;
+        displayManager.sddm.enable = true;
+        # displayManager = {
+        #     startx.enable = true;
+        #     session = [{ manage="window"; name="i3"; start=''$HOME/.xsession''; }];
+        # };
     };
-    services.displayManager = {
-        defaultSession = "none+i3";
-        autoLogin = {
-            enable = false;
-            user = "neg";
-        };
-    };
+    # services.displayManager = {
+    #     defaultSession = "none+i3";
+    #     autoLogin = {
+    #         enable = false;
+    #         user = "neg";
+    #     };
+    # };
 }
