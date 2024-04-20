8d3908bb3ee1da5fea972bb4e6c7f4deef0f7854
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-20 03:00:40 +0300

    [trash] update

--- pkgs/trash.nix
+++ pkgs/trash.nix
@@ -1,9 +1,12 @@
 { pkgs, ... }: {
     environment.systemPackages = with pkgs; [
-        # hddtemp # display hard disk temperature
-        # hdparm # set ata/sata params
+        # chez # Chez Scheme (useful for idris)
+        # cosmocc # Cosmopolitan (Actually Portable Executable) C/C++ toolchain; use via CC=cosmocc, CXX=cosmoc++
         # flatpak-builder # build flatpaks
         # hashcat # password recovery
+        # hddtemp # display hard disk temperature
+        # hdparm # set ata/sata params
+        # idris2 # Idris2 functional statically-typed programming language that looks cool and compiles to C
         # qFlipper # desktop stuff for flipper zero
     ];
 }
