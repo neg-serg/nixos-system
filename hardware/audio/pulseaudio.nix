fc274915a9c4120709f594911b626313dd9d1011
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-21 21:20:59 +0300

    [pulseaudio] with tsched=0 it works bad

--- hardware/audio/pulseaudio.nix
+++ hardware/audio/pulseaudio.nix
@@ -1,8 +1,4 @@
-{pkgs, ...}: {
+{...}: {
   hardware.pulseaudio.enable = true;
   hardware.pulseaudio.support32Bit = true;
-  hardware.pulseaudio.configFile = pkgs.runCommand "default.pa" {} ''
-    sed 's/module-udev-detect$/module-udev-detect tsched=0/' \
-    ${pkgs.pulseaudio}/etc/pulse/default.pa > $out
-  '';
 }
