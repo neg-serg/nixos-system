3c2f82e65f76fd44600e81115395666d4e530cfd
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-22 02:00:07 +0300

    Revert "[kernel] try fix, again"
    This reverts commit b8dd4bb765cb52cbffeda3f4ed2f2e604cb84842.

--- system/kernel.nix
+++ system/kernel.nix
@@ -131,17 +131,15 @@ in
       ++ video_settings;
     boot.extraModulePackages = [];
     boot.consoleLogLevel = 1;
-    boot.kernelPackages = pkgs.linuxPackages_latest;
+    boot.kernelPackages = pkgs.linuxPackages_cachyos-lto-custom;
     nixpkgs.overlays = [
       (self: super: {
         linuxPackages_cachyos-lto-custom = pkgs.linuxPackagesFor (pkgs.linuxPackages_cachyos-lto.kernel.override {
           structuredExtraConfig = with lib.kernel; {
             HZ_1000 = yes;
             HZ_PERIODIC = yes;
-            NO_HZ = no;
-            NO_HZ_IDLE = no;
-            NO_HZ_FULL = no;
           };
+          ignoreConfigErrors = true;
         });
       })
     ];
