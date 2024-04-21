1d3985a6d8ae7458fd41ce491343ccd7269ce8f4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-21 20:34:51 +0300

    [pulseaudio] try new settings

--- hardware/audio/pulseaudio.nix
+++ hardware/audio/pulseaudio.nix
@@ -1,4 +1,8 @@
-{...}: {
-    hardware.pulseaudio.enable = true;
-    hardware.pulseaudio.support32Bit = true;
+{pkgs, ...}: {
+  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.support32Bit = true;
+  hardware.pulseaudio.configFile = pkgs.runCommand "default.pa" {} ''
+    sed 's/module-udev-detect$/module-udev-detect tsched=0/' \
+    ${pkgs.pulseaudio}/etc/pulse/default.pa > $out
+  '';
 }
