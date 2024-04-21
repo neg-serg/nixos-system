56b4ad69afd575acb95a2c18ff2eb80d4a012b14
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-21 15:49:26 +0300

    [games] CS

--- user/games.nix
+++ user/games.nix
@@ -1,24 +1,25 @@
-{ ... }: {
-    nixpkgs.config.packageOverrides = pkgs: {
-        steam = pkgs.steam.override {
-            extraPkgs = pkgs: with pkgs; [
-                keyutils
-                    libkrb5
-                    libpng
-                    libpulseaudio
-                    libvorbis
-                    stdenv.cc.cc.lib
-                    xorg.libXcursor
-                    xorg.libXi
-                    xorg.libXinerama
-                    xorg.libXScrnSaver
-            ];
-        };
+{...}: {
+  nixpkgs.config.packageOverrides = pkgs: {
+    steam = pkgs.steam.override {
+      extraPkgs = pkgs:
+        with pkgs; [
+          keyutils
+          libkrb5
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+        ];
     };
-    programs.steam = {
-        enable = true;
-        dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-        gamescopeSession.enable = true;
-        remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    };
+  };
+  programs.steam = {
+    enable = true;
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    gamescopeSession.enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  };
 }
