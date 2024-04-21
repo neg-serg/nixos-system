badde3f0af0d92a6522cd05012fcb5fd80b51d77
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-21 14:48:05 +0300

    [init] CS

--- init.nix
+++ init.nix
@@ -1,10 +1,10 @@
 # Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 {
-    imports = [ ./system ./hardware ./nix ./user ./pkgs ];
-    system = {
-        stateVersion = "23.11"; # (man configuration.nix or on https://nixos.org/nixos/options.html).
-        autoUpgrade.enable = true;
-        autoUpgrade.allowReboot = true;
-    };
+  imports = [./system ./hardware ./nix ./user ./pkgs];
+  system = {
+    stateVersion = "23.11"; # (man configuration.nix or on https://nixos.org/nixos/options.html).
+    autoUpgrade.enable = true;
+    autoUpgrade.allowReboot = true;
+  };
 }
