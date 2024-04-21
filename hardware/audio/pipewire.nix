c58959dd9e77da2a0137a192d876af582ad37b0a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-21 13:32:58 +0300

    [pipewire] reformat

--- hardware/audio/pipewire.nix
+++ hardware/audio/pipewire.nix
@@ -1,10 +1,17 @@
-{ ... }: {
-    security.rtkit.enable = true;
-    services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-        jack.enable = true;
-    };
+{...}: {
+  nixpkgs.overlays = [
+    (final: prev: {
+      pipewire = prev.pipewire.override {
+        stdenv = prev.stdenvAdapters.impureUseNativeOptimizations prev.gcc13Stdenv;
+      };
+    })
+  ];
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
 }
