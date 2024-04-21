63c5e5f62fc4843ddd20a5eb5512db0a659eda17
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-21 15:51:49 +0300

    [games] better sysctl

--- pkgs/games.nix
+++ pkgs/games.nix
@@ -1,8 +1,9 @@
 { pkgs, inputs, ... }: {
     boot.kernel.sysctl = {
-        # NixOS configuration for Star Citizen requirements
-        "vm.max_map_count" = 16777216;
         "fs.file-max" = 524288;
+        "kernel.sched_cfs_bandwidth_slice_us" = 3000; # better settings for cfs
+        "net.ipv4.tcp_fin_timeout" = 5; # decrease default fin timeout
+        "vm.max_map_count" = 16777216; # need for some games
     };
     environment.systemPackages = with pkgs; [
         (inputs.nix-gaming.packages.${hostPlatform.system}.star-citizen.override {
