6c97cd27c2d9a3b716a315f940e124c580ab13dc
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-04-29 16:09:32 +0300

    [pipewire] try optimized version

--- hardware/audio/pipewire.nix
+++ hardware/audio/pipewire.nix
@@ -1,4 +1,11 @@
 {master, ...}: {
+  nixpkgs.overlays = [
+    (final: prev: {
+      pipewire = prev.pipewire.override {
+        stdenv = prev.stdenvAdapters.impureUseNativeOptimizations prev.gcc13Stdenv;
+      };
+    })
+  ];
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
