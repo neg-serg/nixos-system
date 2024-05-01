ca0e4a2b676ba3737f3a9ac3629b40a1f1b601d5
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-05-01 12:05:18 +0300

    [users] add hashed password

--- system/users.nix
+++ system/users.nix
@@ -5,6 +5,7 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPKg+t07fFxKPqtDR3rRpvS6Tc9Rrh5yv7fC5GFrBtyK neg@telfir"
       ];
+      hashedPassword = "$6$dy0VIe3yFCwdxh2q$CkgfitafpeqFwYn7fqmR/CRP29G/h9sBcN8sOPSXfgkCeraXA6B8VOoJLrsaktUsSnfHbC0RqDHcnnUCtICF4.";
       description = "Neg";
       extraGroups = [
         "audio"
