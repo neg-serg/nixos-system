79defa954cf5420fcfa2d6f1c911fb79be20d07c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-05-11 01:39:48 +0300

    [games] cleanup

--- user/games.nix
+++ user/games.nix
@@ -1,21 +1,4 @@
 {...}: {
-  nixpkgs.config.packageOverrides = pkgs: {
-    steam = pkgs.steam.override {
-      extraPkgs = pkgs:
-        with pkgs; [
-          keyutils
-          libkrb5
-          libpng
-          libpulseaudio
-          libvorbis
-          stdenv.cc.cc.lib
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXinerama
-          xorg.libXScrnSaver
-        ];
-    };
-  };
   programs.steam = {
     enable = true;
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
