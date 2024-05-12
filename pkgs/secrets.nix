fe5a591238d605511497ed417c7090aabc6b60e7
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-05-13 02:05:02 +0300

    [secrets] cleanup

--- pkgs/secrets.nix
+++ pkgs/secrets.nix
@@ -1,22 +1,9 @@
 {pkgs, ...}: {
-  nixpkgs.overlays = [
-    (
-      final: prev: {
-        pcsclite = prev.pcsclite.overrideAttrs (old: {
-          postPatch = ''
-            substituteInPlace src/libredirect.c src/spy/libpcscspy.c \
-              --replace-fail "libpcsclite_real.so.1" "$lib/lib/libpcsclite_real.so.1"
-          '';
-        });
-      }
-    )
-  ];
   environment.systemPackages = with pkgs; [
+    age # pgp alternative
     opensc # libraries and utilities to access smart cards
     p11-kit # loading and sharing PKCS#11 modules
     pcsctools # tools used to test a PC/SC driver, card or reader
-
     sops # secret management
-    age # pgp alternative
   ];
 }
