00cc0e72572bb9aa5d0e956cce84cf0cdfab1dcb
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-05-12 23:08:56 +0300

    [secrets] try fix libpcsclite

--- pkgs/secrets.nix
+++ pkgs/secrets.nix
@@ -1,4 +1,16 @@
 {pkgs, ...}: {
+  nixpkgs.overlays = [
+    (
+      final: prev: {
+        pcsclite = prev.pcsclite.overrideAttrs (old: {
+          postPatch = ''
+            substituteInPlace src/libredirect.c src/spy/libpcscspy.c \
+              --replace-fail "libpcsclite_real.so.1" "$lib/lib/libpcsclite_real.so.1"
+          '';
+        });
+      }
+    )
+  ];
   environment.systemPackages = with pkgs; [
     opensc # libraries and utilities to access smart cards
     p11-kit # loading and sharing PKCS#11 modules
