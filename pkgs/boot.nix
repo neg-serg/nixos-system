8507ff729efc7d2b6883697982f4bedae7f7fdfa
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-05-26 02:17:31 +0300

    [boot] merge with pkgs

--- pkgs/boot.nix
+++
@@ -1,7 +0,0 @@
-{pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    efibootmgr # rule efi boot
-    efivar # manipulate efi vars
-    os-prober # utility to detect other OSs on a set of drives
-  ];
-}
--- system/boot.nix
+++ system/boot.nix
@@ -1,6 +1,8 @@
 {pkgs, ...}: {
-  imports = [
-    ../pkgs/boot.nix
+  environment.systemPackages = with pkgs; [
+    efibootmgr # rule efi boot
+    efivar # manipulate efi vars
+    os-prober # utility to detect other OSs on a set of drives
   ];
   boot = {
     loader = {
