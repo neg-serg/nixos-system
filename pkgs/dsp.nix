f256a084c4fcbf2f66e3add030e37cec593182a6
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-02 02:25:03 +0300

    [dsp] add brutefir

--- pkgs/dsp.nix
+++ pkgs/dsp.nix
@@ -3,6 +3,7 @@
   ...
 }: {
   environment.systemPackages = with pkgs; [
+    brutefir # one of the best FIR filters for linux
     carla # audio plugin host
     jamesdsp # pipewire dsp
     lsp-plugins # various linux dsp
