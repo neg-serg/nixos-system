bdb43727d1a4ac97dbc07c7c5342c3ccd274d8e2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-02 01:28:31 +0300

    [virt] add wine

--- system/virt.nix
+++ system/virt.nix
@@ -1,3 +1,8 @@
-{...}: {
+{pkgs, ...}: {
   virtualisation.docker.enable = true;
+  environment.systemPackages = with pkgs; [
+      dxvk # for plugins compatibility
+      wine-staging # tool to run windows packages
+      winetricks # stuff to install dxvk
+  ];
 }
