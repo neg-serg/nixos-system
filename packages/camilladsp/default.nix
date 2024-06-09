b180a703a843d10a1f843fc405782b4e764500bc
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-10 00:41:38 +0300

    [dsp] try camilladsp

---
+++ packages/camilladsp/default.nix
@@ -0,0 +1,56 @@
+{ lib
+, rustPlatform
+, fetchFromGitHub
+, pkg-config
+, bzip2
+, libpulseaudio
+, openssl
+, stdenv
+, darwin
+, alsa-lib
+}:
+
+rustPlatform.buildRustPackage rec {
+  pname = "camilladsp";
+  version = "2.0.3";
+
+  src = fetchFromGitHub {
+    owner = "HEnquist";
+    repo = "camilladsp";
+    rev = "v${version}";
+    hash = "sha256-E/KlwXoKvkuMPEKAZv/6l0F1KikZNFpw/9Iiw+Z8q/I=";
+  };
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+  };
+
+  postPatch = ''
+    ln -s ${./Cargo.lock} Cargo.lock
+  '';
+
+  nativeBuildInputs = [
+    pkg-config
+    rustPlatform.bindgenHook
+  ];
+
+  buildInputs = [
+    bzip2
+    libpulseaudio
+    openssl
+  ] ++ lib.optionals stdenv.isDarwin [
+    darwin.apple_sdk.frameworks.CoreAudio
+    darwin.apple_sdk.frameworks.Security
+  ] ++ lib.optionals stdenv.isLinux [
+    alsa-lib
+  ];
+
+  meta = with lib; {
+    description = "A flexible cross-platform IIR and FIR engine for crossovers, room correction etc";
+    homepage = "https://github.com/HEnquist/camilladsp";
+    changelog = "https://github.com/HEnquist/camilladsp/blob/${src.rev}/CHANGELOG.md";
+    license = licenses.gpl3Only;
+    maintainers = with maintainers; [ ];
+    mainProgram = "camilladsp";
+  };
+}
--- pkgs/dsp.nix
+++ pkgs/dsp.nix
@@ -1,9 +1,13 @@
 {
   pkgs,
   ...
-}: {
+}:
+with {
+    camilladsp = pkgs.callPackage ../packages/camilladsp {};
+};{
   environment.systemPackages = with pkgs; [
     brutefir # one of the best FIR filters for linux
+    camilladsp # one of the best DSP for linux
     carla # audio plugin host
     jamesdsp # pipewire dsp
     lsp-plugins # various linux dsp
