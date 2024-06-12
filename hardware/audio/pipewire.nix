800efe43ec85b5eb2e2e0b1a12b094ef2741c30d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-12 21:07:34 +0300

    [wireplumber] try add some global settings

--- hardware/audio/pipewire.nix
+++ hardware/audio/pipewire.nix
@@ -6,8 +6,21 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
-    wireplumber.package = pkgs.wireplumber;
+    wireplumber = {
+      package = pkgs.wireplumber;
+      extraConfig = {
+        # Tell wireplumber to be more verbose
+        "10-log-level-debug" = {
+          "context.properties"."log.level" = "D"; # output debug logs
+        };
+        # Default volume is by default set to 0.4 instead set it to 1.0
+        "10-default-volume" = {
+          "wireplumber.settings"."device.routes.default-sink-volume" = 1.0;
+        };
+      };
+    };
   };
+
   # run pipewire on default.target, this fixes xdg-portal startup delay
-  systemd.user.services.pipewire.wantedBy = [ "default.target" ];
+  systemd.user.services.pipewire.wantedBy = ["default.target"];
 }
