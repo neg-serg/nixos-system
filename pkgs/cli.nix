b6cb37bbf36e2c9104883d4491b3bfd60b2320bf
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-12 23:42:51 +0300

    [cli] use default versions

--- pkgs/cli.nix
+++ pkgs/cli.nix
@@ -1,7 +1,6 @@
 {
   pkgs,
   stable,
-  master,
   ...
 }: {
   environment.systemPackages = with pkgs; [
@@ -9,21 +8,21 @@
     delta # better diff tool
     difftastic # syntax-aware diff
     diffutils # classic diff utils
-    master.fd # better find
+    eza # more modern version of exa ls replacer
+    fd # better find
     file # get filetype from content
     inotify-tools # shell inotify bindings
     lsof # list open something
     parallel # parallel xargs
     procps # info about processes using /proc
-    master.progress # show progress over all coreutils
+    progress # show progress over all coreutils
     psmisc # killall and friends
     pv # pipe viewer
     readline # readline library
-    master.reptyr # move app to another pty, tmux as an example ( echo 0 > /proc/sys/kernel/yama/ptrace_scope )
-    master.ripgrep # better grep
+    reptyr # move app to another pty, tmux as an example ( echo 0 > /proc/sys/kernel/yama/ptrace_scope )
+    ripgrep # better grep
     rlwrap # readline wrapper for everything
-    master.eza # more modern version of exa ls replacer
     stable.tig # git viewer
-    master.tmux # better screen
+    tmux # better screen
   ];
 }
