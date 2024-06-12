7856f48fa4d17fa9b59dd2ecb58255c442fe0998
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-12 23:13:18 +0300

    Revert "[virt] add binfmt interpretators"
    This reverts commit b53c13c39cc3f61b3b9f8e52e0fa1f76dbff9b77.

--- system/virt.nix
+++ system/virt.nix
@@ -12,11 +12,4 @@
       wine-staging # tool to run windows packages
       winetricks # stuff to install dxvk
   ];
-  nix.settings.extra-sandbox-paths = ["/run/binfmt" "${pkgs.qemu}"];
-  boot.binfmt = {
-    registrations = {
-      aarch64-linux.interpreter = "${pkgs.qemu}/bin/qemu-aarch64"; # aarch64 interpreter
-      i686-linux.interpreter = "${pkgs.qemu}/bin/qemu-i686"; # i686 interpreter
-    };
-  };
 }
