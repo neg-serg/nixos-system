e5d76e6e11e79dde8e26356d542b01fe2b32d3e8
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-12 23:10:35 +0300

    [oomd] try new settings

--- system/default.nix
+++ system/default.nix
@@ -1,10 +1,11 @@
 {
   imports = [
     ./boot.nix
+    ./environment.nix
     ./filesystems.nix
     ./kernel.nix
-    ./environment.nix
     ./networking.nix
+    ./oomd.nix
     ./security.nix
     ./systemd.nix
     ./users.nix
