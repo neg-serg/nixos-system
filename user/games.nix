a5c5bbdbdb978bb2231e4e4b2156037ccd0ebf40
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-12 17:14:20 +0300

    [games] add gamescope stuff

--- user/games.nix
+++ user/games.nix
@@ -1,8 +1,25 @@
-{...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   programs.steam = {
     enable = true;
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     gamescopeSession.enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  };
+  programs.gamescope = {
+    enable = true;
+    package = pkgs.gamescope; # the default, here in case I want to override it
+  };
+
+  # workaround attempt for letting gamescope bypass YAMA LSM
+  # doesn't work, but doesn't hurt to keep this here
+  security.wrappers.gamescope = {
+    owner = "root";
+    group = "root";
+    source = "${config.programs.gamescope.package}/bin/gamescope";
+    capabilities = "cap_sys_ptrace,cap_sys_nice+pie";
   };
 }
