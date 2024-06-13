3f8e93f454708ecbb47e9f58e11211de650186b5
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-14 00:27:10 +0300

    [xdg] use portal

--- user/xdg.nix
+++ user/xdg.nix
@@ -6,6 +6,9 @@
       xdg-desktop-portal-gtk
       xdg-desktop-portal-wlr
     ];
+    # Sets environment variable NIXOS_XDG_OPEN_USE_PORTAL to 1 This will make xdg-open use the portal to open programs, which resolves bugs
+    # involving programs opening inside FHS envs or with unexpected env vars set from wrappers. See #160923 for more info.
+    xdgOpenUsePortal = true;
     wlr = {enable = true;};
     config.common.default = "wlr";
   };
