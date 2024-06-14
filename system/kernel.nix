1f8f378ea93b699e2d235167971a3be2159e682c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-14 05:08:25 +0300

    [kernel] add low-latency tuning

--- system/kernel.nix
+++ system/kernel.nix
@@ -4,7 +4,7 @@
   kexec_enabled,
   ...
 }: let
-  inherit (lib.kernel) yes no;
+  inherit (lib.kernel) yes no freeform;
   inherit (lib.attrsets) mapAttrs;
   inherit (lib.modules) mkForce;
   # Disables all security mitigations. This can significantly improve performance, but it can also make the system very vulnerable to security attacks.
@@ -147,6 +147,32 @@ in {
         LRU_GEN = yes; # multigen LRU
         LRU_GEN_ENABLED = yes;
         CPU_FREQ_STAT = yes; # collect CPU frequency statistics
+
+        HZ = freeform "1000";
+        HZ_1000 = yes;
+
+        PREEMPT = yes;
+        PREEMPT_BUILD = yes;
+        PREEMPT_COUNT = yes;
+        PREEMPT_VOLUNTARY = no;
+        PREEMPTION = yes;
+
+        TREE_RCU = yes;
+        PREEMPT_RCU = yes;
+        RCU_EXPERT = yes;
+        TREE_SRCU = yes;
+        TASKS_RCU_GENERIC = yes;
+        TASKS_RCU = yes;
+        TASKS_RUDE_RCU = yes;
+        TASKS_TRACE_RCU = yes;
+        RCU_STALL_COMMON = yes;
+        RCU_NEED_SEGCBLIST = yes;
+        RCU_FANOUT = freeform "64";
+        RCU_FANOUT_LEAF = freeform "16";
+        RCU_BOOST = yes;
+        RCU_BOOST_DELAY = freeform "500";
+        RCU_NOCB_CPU = yes;
+        RCU_LAZY = yes;
       };
     }
   ];
