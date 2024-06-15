7fb0d70aff8d892903ce882ae9cd8463a7552fc2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-16 01:57:40 +0300

    [selinux] try

--- system/kernel.nix
+++ system/kernel.nix
@@ -68,6 +68,7 @@
     "vivid"
   ];
 in {
+  imports = [ ./selinux.nix ];
   # thx to https://github.com/hlissner/dotfiles
   boot.kernel.sysctl = {
     # The Magic SysRq key is a key combo that allows users connected to the
