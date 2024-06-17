9edeada75ffbb57b85b969e0c13b499d088ee228
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-18 01:28:36 +0300

    [git] add git-crypt

--- pkgs/git.nix
+++ pkgs/git.nix
@@ -1,8 +1,9 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
-    git # my favorite dvcs
+    git-crypt # git-based incryption
     git-extras # git extra stuff
     git-filter-repo # quickly rewrite git history
     git-lfs # git extension for large files
+    git # my favorite dvcs
   ];
 }
