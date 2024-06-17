b53d353fb20c4c046bd08ff7a34f0655138b0e34
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-18 02:41:15 +0300

    [system] add cryptsetup

--- pkgs/system.nix
+++ pkgs/system.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
+    cryptsetup # stuff for LUKS
     dmidecode # extract system/memory/bios info
     kexec-tools # tools related to the kexec Linux feature
     lm_sensors # sensors
