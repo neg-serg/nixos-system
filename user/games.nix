c8fba01baca04a7717749df0dde81361dc6e1052
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-06-22 19:09:05 +0300

    [gamescope] disable for now

--- user/games.nix
+++ user/games.nix
@@ -6,22 +6,22 @@
   programs.steam = {
     enable = true;
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    gamescopeSession.enable = true;
+    gamescopeSession.enable = false;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
   };
   programs.gamescope = {
-    enable = true;
+    enable = false;
     package = pkgs.gamescope; # the default, here in case I want to override it
   };
 
-  # workaround attempt for letting gamescope bypass YAMA LSM
-  # doesn't work, but doesn't hurt to keep this here
-  security.wrappers.gamescope = {
-    owner = "root";
-    group = "root";
-    source = "${config.programs.gamescope.package}/bin/gamescope";
-    capabilities = "cap_sys_ptrace,cap_sys_nice+pie";
-  };
+  # # workaround attempt for letting gamescope bypass YAMA LSM
+  # # doesn't work, but doesn't hurt to keep this here
+  # security.wrappers.gamescope = {
+  #   owner = "root";
+  #   group = "root";
+  #   source = "${config.programs.gamescope.package}/bin/gamescope";
+  #   capabilities = "cap_sys_ptrace,cap_sys_nice+pie";
+  # };
 
   security.wrappers.gamemode = {
     owner = "root";
