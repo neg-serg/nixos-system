940c33ab10d1e4322da845284998f7d2a401681f
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-06 00:45:14 +0300

    [pkgs] network: add wireguard tools

--- pkgs/network.nix
+++ pkgs/network.nix
@@ -2,6 +2,7 @@
   environment.systemPackages = with pkgs; [
     (openvpn.override {pkcs11Support=true; pkcs11helper=pkgs.pkcs11helper;})
     update-resolv-conf # /etc/resolv.conf with DNS settings that come from the received push dhcp-options
+    wireguard-tools
 
     bandwhich # display network utilization per process
     cacert # for curl certificate verification
