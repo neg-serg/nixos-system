34e59b551a1a411276b038286dc4b22d84bb4c92
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-05 23:43:28 +0300

    [pkgs] add some new daemons

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -50,11 +50,16 @@
       enable = true;
       nssmdns4 = true;
     };
+
+    accounts-daemon.enable = true;
     chrony.enable = true;
     dbus.implementation = "broker";
+    devmon.enable = true;
     earlyoom.enable = false; # may need it for notebook
     fwupd.enable = true;
+    gnome = { gnome-keyring.enable = true; };
     gvfs.enable = true;
+    power-profiles-daemon.enable = true;
     jellyfin = {
         enable = true;
         openFirewall = true;
