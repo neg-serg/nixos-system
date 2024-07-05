1e522abde7afb9a06bf0b759200ae0bb7dab9adb
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-06 00:46:27 +0300

    Revert "[pkgs] remove protonvpn gui"
    This reverts commit 785d73e0e8bd6b6adddf35dfb7a610e11065d2fd.

--- pkgs/network.nix
+++ pkgs/network.nix
@@ -1,6 +1,7 @@
 {pkgs, stable, ...}: {
   environment.systemPackages = with pkgs; [
     (openvpn.override {pkcs11Support=true; pkcs11helper=pkgs.pkcs11helper;})
+    protonvpn-gui # protonvpn
     update-resolv-conf # /etc/resolv.conf with DNS settings that come from the received push dhcp-options
     wireguard-tools
 
