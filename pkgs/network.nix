0b819c571598d07e3457789465623ec577b53f47
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-05 15:58:59 +0300

    [network] add update-resolv-conf

--- pkgs/network.nix
+++ pkgs/network.nix
@@ -2,6 +2,7 @@
   environment.systemPackages = with pkgs; [
     (openvpn.override {pkcs11Support=true; pkcs11helper=pkgs.pkcs11helper;})
     protonvpn-gui # protonvpn
+    update-resolv-conf # /etc/resolv.conf with DNS settings that come from the received push dhcp-options
 
     bandwhich # display network utilization per process
     cacert # for curl certificate verification
