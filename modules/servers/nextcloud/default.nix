47ae667315f0af032fcf44e8a5a5fcdf35ffdc29
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-07 13:55:47 +0300

    [nextcloud] make it work

--- modules/servers/nextcloud/default.nix
+++ modules/servers/nextcloud/default.nix
@@ -1,9 +1,16 @@
 {pkgs, ...}: {
-  environment.etc."nextcloud-admin-pass".text = "PWD";
+  environment.etc."nextcloud/admin-pass".text = "eizoo4queegobaeFe7fae0eica9xeecea9Uu1vu4ar0gohyo";
   services.nextcloud = {
     enable = true;
     package = pkgs.nextcloud28;
     hostName = "localhost";
-    config.adminpassFile = "/etc/nextcloud-admin-pass";
+    database.createLocally = true;
+    configureRedis = true;
+    datadir = "/nextcloud";
+    config = {
+      adminuser = "init";
+      adminpassFile = "/etc/nextcloud/admin-pass";
+      dbtype = "mysql";
+    };
   };
 }
