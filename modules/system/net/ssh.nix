010265a2762cfd58d2bb3e7bd8978cf96993d931
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-12 00:12:48 +0300

    [opensc] use master

--- modules/system/net/ssh.nix
+++ modules/system/net/ssh.nix
@@ -1,12 +1,13 @@
-{pkgs, ...}: {
+{master, ...}: {
   programs = {
     ssh = {
+      package = master.openssh;
       startAgent = true;
       # agentPKCS11Whitelist = "/nix/store/*";
       # or specific URL if you're paranoid
       # but beware this can break if you don't have exactly matching opensc versions
       # between your main config and home-manager channel
-      agentPKCS11Whitelist = "${pkgs.opensc}/lib/opensc-pkcs11.so";
+      agentPKCS11Whitelist = "${master.opensc}/lib/opensc-pkcs11.so";
     };
   };
 }
