0592740c2b9aba67bfe002816db1645d4ea20574
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-07-22 18:52:51 +0300

    [flatpak] add some new apps

--- modules/flatpak/default.nix
+++ modules/flatpak/default.nix
@@ -1,6 +1,11 @@
 {pkgs, ...}: {
   environment.systemPackages = [pkgs.flatpak];
   services.flatpak.enable = true;
-  services.flatpak.packages = [ { appId = "us.zoom.Zoom"; origin = "flathub";  } ];
+  services.flatpak.packages = [
+    {appId = "us.zoom.Zoom"; origin = "flathub";} 
+    {appId = "com.obsproject.Studio"; origin = "flathub";} 
+    {appId = "org.onlyoffice.desktopeditors"; origin = "flathub";} 
+    {appId = "com.usebottles.bottles"; origin = "flathub";}
+  ];
   services.flatpak.update.onActivation = true;
 }
