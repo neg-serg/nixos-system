501dc2673aed18166e2256b116b5b675541e5887
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-08-03 23:41:04 +0300

    [systemd] enableUnifiedCgroupHierarchy is removed now

--- modules/system/oomd.nix
+++ modules/system/oomd.nix
@@ -1,5 +1,4 @@
 {
-  config,
   lib,
   ...
 }: {
@@ -12,7 +11,7 @@
     # and system slices, but their oomd configuration has since changed.
     # TODO: maybe disable user slice by default?
     oomd = {
-      enable = !config.systemd.enableUnifiedCgroupHierarchy;
+      enable = true;
       enableRootSlice = true;
       enableSystemSlice = true;
       enableUserSlices = true;
