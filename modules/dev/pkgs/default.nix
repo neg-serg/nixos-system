02dc78bdf75d2803e4f194cf415c3888f1f9654b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-08-21 21:38:39 +0300

    [dev] CS

--- modules/dev/pkgs/default.nix
+++ modules/dev/pkgs/default.nix
@@ -4,20 +4,20 @@
   ...
 }: {
   environment.systemPackages = with pkgs; [
-    ddrescue # data recovery tool
     bpftrace # add tool to trace events via bpf
+    ddrescue # data recovery tool
     foremost # files extact from structure
     gcc # gnu compiler collection
     gdb # gnu debugger
     hexyl # command-line hex editor
     hyperfine # command-line benchmarking tool
     imhex # gui hex editor
-    stable.julia # nice language
     ltrace # trace functions
     pkgconf # package compiler and linker metadata toolkit (wrapper script)
     plow # high performance http benchmarking tool
     radare2-cutter # Free and Open Source Reverse Engineering Platform powered by rizin
     radare2 # free disassembler
+    stable.julia # nice language
     strace # trace system-calls
   ];
 }
