8246360358dd832949c2b644fc63538302925a00
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-08-25 21:49:04 +0300

    [keychron] add udev rules

--- modules/hardware/udev-rules/default.nix
+++ modules/hardware/udev-rules/default.nix
@@ -2,5 +2,6 @@
   services.udev.extraRules = ''
     DEVPATH=="/devices/virtual/misc/cpu_dma_latency", OWNER="root", GROUP="audio", MODE="0660"
     ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="none"
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0350", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"'
   '';
 }
