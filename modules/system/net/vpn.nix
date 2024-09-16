584067f39e21285f814112e89c0f5d2fa319900c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-09-16 16:40:17 +0300

    [protonvpn] remove

--- modules/system/net/vpn.nix
+++ modules/system/net/vpn.nix
@@ -4,7 +4,6 @@
       pkcs11Support = true;
       pkcs11helper = pkgs.pkcs11helper;
     })
-    protonvpn-gui # protonvpn
     update-resolv-conf # /etc/resolv.conf with DNS settings that come from the received push dhcp-options
     wireguard-tools # stuff to rule wireguard
   ];
