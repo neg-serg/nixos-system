9b3a3d073802788c3b743eee64a7e88063c4cf0d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-10-22 23:48:48 +0300

    [amd] CS

--- modules/hardware/video/amd/default.nix
+++ modules/hardware/video/amd/default.nix
@@ -17,8 +17,8 @@
         output = "DisplayPort-1";
         monitorConfig = ''
           # Find these values in "$HOME/.local/share/xorg/Xorg.0.log They will be in this exact format so scroll until you find it.
-          Modeline    "3440x1440_175"  1019.75  3440 3488 3520 3600  1440 1443 1453 1619 +hsync -vsync
-          Option		"PreferredMode"	"3440x1440_175"
+          Modeline  "3440x1440_175"  1019.75  3440 3488 3520 3600  1440 1443 1453 1619 +hsync -vsync
+          Option	"PreferredMode"	"3440x1440_175"
         '';
       }
     ];
@@ -30,7 +30,7 @@
     '';
     deviceSection = ''
       Option "VariableRefresh" "false"
-      Option "EnablePageFlip" "off"
+      Option "EnablePageFlip"  "off"
     '';
   };
   services.xserver.videoDrivers = ["amdgpu"];
