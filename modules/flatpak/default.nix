ba2606de64ad0247fe01251c2fec033475316cc1
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-10-23 03:29:10 +0300

    [flatpak] CS

--- modules/flatpak/default.nix
+++ modules/flatpak/default.nix
@@ -2,10 +2,22 @@
   environment.systemPackages = [pkgs.flatpak pkgs.flatpak-builder];
   services.flatpak.enable = true;
   services.flatpak.packages = [
-    {appId = "us.zoom.Zoom"; origin = "flathub";} 
-    {appId = "com.obsproject.Studio"; origin = "flathub";} 
-    {appId = "org.onlyoffice.desktopeditors"; origin = "flathub";} 
-    {appId = "com.usebottles.bottles"; origin = "flathub";}
+    {
+      appId = "us.zoom.Zoom";
+      origin = "flathub";
+    }
+    {
+      appId = "com.obsproject.Studio";
+      origin = "flathub";
+    }
+    {
+      appId = "org.onlyoffice.desktopeditors";
+      origin = "flathub";
+    }
+    {
+      appId = "com.usebottles.bottles";
+      origin = "flathub";
+    }
   ];
   services.flatpak.update.onActivation = true;
 }
