453d1dee411038ec908999b630020b8abf08140a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-11-02 02:32:16 +0300

    [vpn] cleanup

--- modules/system/net/vpn/default.nix
+++ modules/system/net/vpn/default.nix
@@ -1,29 +1,20 @@
-{pkgs, ...}:
-with {
-  amneziawg-module = pkgs.callPackage ../../linux/amneziawg {
-    kernel = pkgs.linuxPackages_6_11.kernel;
-  };
-  amneziawg-tools = pkgs.callPackage ../vpn/packages/amneziawg-tools {
-    amneziawg-go = pkgs.callPackage ../vpn/packages/amneziawg-go {};
-  };
-}; {
+{pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     (openvpn.override {
       pkcs11Support = true;
       pkcs11helper = pkgs.pkcs11helper;
     })
-    amneziawg-module # kernel module for amneziawg
-    amneziawg-tools # tools for amneziawg
     openconnect # ciscoanyconnect open source
     update-resolv-conf # /etc/resolv.conf with DNS settings that come from the received push dhcp-options
-    wireguard-tools # wireguard interface
   ];
+
   services.openvpn.servers = {
     work = {
       config = ''config /home/neg/.dotfiles/nix/.config/home-manager/secrets/crypted/work.ovpn '';
       autoStart = false;
     };
   };
+
   systemd.paths.openvpn-work = {
     enable = true;
     description = "OpenVPN for work activation path";
@@ -32,4 +23,5 @@ with {
     };
     wantedBy = ["multi-user.target"];
   };
+
 }
