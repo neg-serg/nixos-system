8bb564d864ebed1ddfd48eaaa090b12ba3d353c4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-11-04 02:16:57 +0300

    [cli] make some stuff systemwide

--- modules/cli/default.nix
+++ modules/cli/default.nix
@@ -6,11 +6,16 @@
     difftastic # syntax-aware diff
     diffutils # classic diff utils
     doggo # dns client for humans
+    dos2unix # file convertation
+    du-dust # better du
+    duf # better df
     eza # more modern version of exa ls replacer
     fd # better find
     file # get filetype from content
     inotify-tools # shell inotify bindings
     lsof # list open something
+    ncdu # interactive du
+    nnn # cli filemanager
     parallel # parallel xargs
     procps # info about processes using /proc
     progress # show progress over all coreutils
@@ -21,6 +26,7 @@
     ripgrep # better grep
     rlwrap # readline wrapper for everything
     rmlint # remove duplicates
+    stow # manage farms of symlinks
     tig # git viewer
   ];
 }
