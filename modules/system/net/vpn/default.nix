18fc4c8f3b80a22261892041945df6d980aa4941
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-11-12 11:51:57 +0300

    [vpn] cleanup

--- modules/system/net/vpn/default.nix
+++ modules/system/net/vpn/default.nix
@@ -5,24 +5,6 @@
       pkcs11helper = pkgs.pkcs11helper;
     })
     openconnect # ciscoanyconnect open source
-    openfortivpn # yet another vpn service
     update-resolv-conf # /etc/resolv.conf with DNS settings that come from the received push dhcp-options
   ];
-
-  services.openvpn.servers = {
-    work = {
-      config = ''config /home/neg/.dotfiles/nix/.config/home-manager/secrets/crypted/work.ovpn '';
-      autoStart = false;
-    };
-  };
-
-  systemd.paths.openvpn-work = {
-    enable = true;
-    description = "OpenVPN for work activation path";
-    pathConfig = {
-      PathChanged = "/run/user/1000/secrets/work.pass";
-    };
-    wantedBy = ["multi-user.target"];
-  };
-
 }
