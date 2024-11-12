ec3e3ea52ce6ace5f136840c114cf9d7402d6bd6
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-11-12 18:18:21 +0300

    [flatpak] try new riot setup

--- modules/flatpak/default.nix
+++ modules/flatpak/default.nix
@@ -2,6 +2,24 @@
   environment.systemPackages = [pkgs.flatpak pkgs.flatpak-builder];
   services.flatpak = {
     enable = true;
+
+    overrides = {
+      global = {
+        Environment = {
+          XCURSOR_PATH = "/run/host/user-share/icons:/run/host/share/icons"; # Fix un-themed cursor in some Wayland apps
+          GTK_THEME = "Adwaita:dark"; # Force correct theme for some GTK apps
+        };
+      };
+
+      "im.riot.Riot".Context = {
+        filesystems = [
+          "/run/current-system/sw/bin:ro"
+          "/home/neg/dw/:rw"
+          "/home/neg/pic/:rw"
+        ];
+      };
+    };
+
     packages = [
       {
         appId = "com.obsproject.Studio";
