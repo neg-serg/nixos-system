ed15b1e0875110f79f6d9e5e1f94e35a59f57ad2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-11-12 19:41:14 +0300

    [xdg] make it fast

--- modules/user/xdg.nix
+++ modules/user/xdg.nix
@@ -1,10 +1,13 @@
 {...}: {
+  # programs.dconf.enable = true;
   xdg.portal = {
     enable = true;
     # Sets environment variable NIXOS_XDG_OPEN_USE_PORTAL to 1 This will make xdg-open use the portal to open programs, which resolves bugs
     # involving programs opening inside FHS envs or with unexpected env vars set from wrappers. See #160923 for more info.
     xdgOpenUsePortal = true;
-    wlr = {enable = true;};
-    config.common.default = "gtk";
+    wlr = {
+      enable = false;
+    };
+    config.common.default = "*";
   };
 }
