847f052f391d8c1299d01523ed67e74e217fe0c0
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-11-14 06:33:00 +0300

    [fonts] new fonts

--- modules/user/fonts.nix
+++ modules/user/fonts.nix
@@ -1,8 +1,9 @@
 {pkgs, ...}: {
   fonts.fontDir.enable = true; # add fontdir support for nixos
   fonts.packages = with pkgs; [
-    (nerdfonts.override {fonts = ["FiraCode"];})
     liberation_ttf # Liberation Fonts, replacements for Times New Roman, Arial, and Courier New
+    (nerdfonts.override {fonts = ["FiraCode"];})
+    proggyfonts # yet another fonts for coding
     symbola # Basic Latin, Greek, Cyrillic and many Symbol blocks of Unicode
     texlivePackages.opensans # The Open Sans font family, and LaTeX support
   ];
