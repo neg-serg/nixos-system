75aa7a79af854a224783a6d4db58530457d61557
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-11-14 06:09:01 +0300

    [fonts] add new fonts

--- modules/user/fonts.nix
+++ modules/user/fonts.nix
@@ -2,5 +2,8 @@
   fonts.fontDir.enable = true; # add fontdir support for nixos
   fonts.packages = with pkgs; [
     (nerdfonts.override {fonts = ["FiraCode"];})
+    liberation_ttf # Liberation Fonts, replacements for Times New Roman, Arial, and Courier New
+    symbola # Basic Latin, Greek, Cyrillic and many Symbol blocks of Unicode
+    texlivePackages.opensans # The Open Sans font family, and LaTeX support
   ];
 }
