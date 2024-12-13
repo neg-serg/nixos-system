fffc0c961f917b334ab0749a9b337de29f4b1bfb
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-12-13 10:53:48 +0300

    [steam] add local network game transfers

--- modules/user/games/default.nix
+++ modules/user/games/default.nix
@@ -5,6 +5,7 @@
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
       gamescopeSession.enable = true;
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
     };
 
     gamescope = {
