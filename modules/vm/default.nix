20c505bd58140395c73d20b0e1553c440ba55210
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-12-18 08:32:42 +0300

    Revert "[vm] remove vmware-horizon"
    This reverts commit 3eca10e51ae5ea6127e1eb0922300bc9b51cce62.

--- modules/vm/default.nix
+++ modules/vm/default.nix
@@ -1,5 +1,5 @@
-{...}: {
-  environment.systemPackages = [
-    # vmware-horizon-client # vmware remote client
+{unstable, ...}: {
+  environment.systemPackages = with unstable; [
+    vmware-horizon-client # vmware remote client
   ];
 }
