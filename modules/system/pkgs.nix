9850fd616f72e3b78589b6a43d210d789250c5e8
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-12-23 20:16:50 +0300

    [system] use stable schedtool

--- modules/system/pkgs.nix
+++ modules/system/pkgs.nix
@@ -1,11 +1,11 @@
-{pkgs, ...}: {
+{stable, pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     cryptsetup # stuff for LUKS
     dmidecode # extract system/memory/bios info
     kexec-tools # tools related to the kexec Linux feature
     lm_sensors # sensors
     pciutils # manipulate pci devices
-    schedtool # CPU scheduling
+    stable.schedtool # CPU scheduling
     usbutils # lsusb
   ];
 }
