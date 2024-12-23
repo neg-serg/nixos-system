8c4acbb74795cc61ae34ae8494580549a5b777ff
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-12-23 20:17:40 +0300

    [video] use stable ddccontrol

--- modules/hardware/video/pkgs/default.nix
+++ modules/hardware/video/pkgs/default.nix
@@ -1,6 +1,6 @@
-{pkgs, ...}: {
+{pkgs, stable, ...}: {
   environment.systemPackages = with pkgs; [
-    ddccontrol # ddc control
+    stable.ddccontrol # ddc control
     ddcutil # rule monitor params
     edid-decode # edid decoder and tester
     read-edid # tool to read and parse edid from monitors
