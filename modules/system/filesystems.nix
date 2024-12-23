7cf4be36458a7f2cbe350876aa8a2c0c53c29108
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-12-23 11:22:36 +0300

    [filesystems] CS

--- modules/system/filesystems.nix
+++ modules/system/filesystems.nix
@@ -19,46 +19,48 @@
     fsType = "vfat";
   };
 
-fileSystems."/zero" = {
-  device = "/dev/mapper/argon-zero";
-  fsType = "xfs";
-  options = [
-    "x-systemd.automount"
-    "relatime"
-    "lazytime"
-    "rw"
-  ];
-};
+  fileSystems."/zero" = {
+    device = "/dev/mapper/argon-zero";
+    fsType = "xfs";
+    options = [
+      "x-systemd.automount"
+      "relatime"
+      "lazytime"
+      "rw"
+    ];
+  };
+  
+  fileSystems."/one" = {
+    device = "/dev/mapper/xenon-one";
+    fsType = "xfs";
+    options = ["x-systemd.automount" "relatime" "lazytime" "rw"];
+  };
+  
+  fileSystems."/home/neg/music" = {
+    device = "/one/music";
+    options = ["bind" "nofail" "x-systemd.automount"];
+  };
+  fileSystems."/home/neg/torrent" = {
+    device = "/one/torrent";
+    options = ["bind" "nofail" "x-systemd.automount"];
+  };
+  fileSystems."/home/neg/vid" = {
+    device = "/one/vid";
+    options = ["bind" "nofail" "x-systemd.automount"];
+  };
+  fileSystems."/home/neg/games" = {
+    device = "/one/games";
+    options = ["bind" "nofail" "x-systemd.automount"];
+  };
+  fileSystems."/home/neg/doc" = {
+    device = "/one/doc";
+    options = ["bind" "nofail" "x-systemd.automount"];
+  };
+  fileSystems."/var/lib/flatpak" = {
+    device = "/one/flatpak";
+    options = ["bind" "nofail" "x-systemd.automount"];
+  };
 
-fileSystems."/one" = {
-  device = "/dev/mapper/xenon-one";
-  fsType = "xfs";
-  options = ["x-systemd.automount" "relatime" "lazytime" "rw"];
-};
-
-fileSystems."/home/neg/music" = {
-  device = "/one/music";
-  options = ["bind" "nofail" "x-systemd.automount"];
-};
-fileSystems."/home/neg/torrent" = {
-  device = "/one/torrent";
-  options = ["bind" "nofail" "x-systemd.automount"];
-};
-fileSystems."/home/neg/vid" = {
-  device = "/one/vid";
-  options = ["bind" "nofail" "x-systemd.automount"];
-};
-fileSystems."/home/neg/games" = {
-  device = "/one/games";
-  options = ["bind" "nofail" "x-systemd.automount"];
-};
-fileSystems."/home/neg/doc" = {
-  device = "/one/doc";
-  options = ["bind" "nofail" "x-systemd.automount"];
-};
-fileSystems."/var/lib/flatpak" = {
-  device = "/one/flatpak";
-  options = ["bind" "nofail" "x-systemd.automount"];
-};
   swapDevices = [];
+
 }
