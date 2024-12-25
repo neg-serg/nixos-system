a47a934b42710ae89adfebe1876c3b1168f35b6a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-12-26 00:46:24 +0300

    [editor] emacs remove

--- modules/dev/editor/default.nix
+++ modules/dev/editor/default.nix
@@ -2,6 +2,5 @@
   programs.nano = {enable = false;}; # I hate nano to be honest
   environment.systemPackages = with pkgs; [
     neovim # better vim
-    emacs # very extensible text editor
   ];
 }
