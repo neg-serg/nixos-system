e0462160209296e6e5430e71367915aedc5192f6
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-12-26 00:38:10 +0300

    [editor] remove vis

--- modules/dev/editor/default.nix
+++ modules/dev/editor/default.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}: {
   programs.nano = {enable = false;}; # I hate nano to be honest
   environment.systemPackages = with pkgs; [
-    vis # minimal editor
     neovim # better vim
     emacs # very extensible text editor
   ];
