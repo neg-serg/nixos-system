88f726306b0b6955e536db2261888c224b93224c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2024-12-31 15:17:21 +0300

    [vm] update settings

--- vm.nix
+++ vm.nix
@@ -1,3 +1,13 @@
 {pkgs, ...}: {
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  virtualisation = {
+    cores = 2;
+    diskSize = 10 * 1024;
+    memorySize = 4 * 1024;
+    qemu = {
+      # networkingOptions = ["-nic bridge,br=br0,model=virtio-net-pci,helper=/run/wrappers/bin/qemu-bridge-helper"];
+      options = [ "-bios" "${pkgs.OVMF.fd}/FV/OVMF.fd" ];
+    };
+  };
+  networking.firewall.enable = false; # for user convenience
 }
