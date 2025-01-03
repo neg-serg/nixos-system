e04be3d277c88bf3200c952af22cd1be499628d3
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-01-03 14:47:17 +0300

    [pulseaudio] remove warning

--- modules/hardware/audio/pulseaudio/default.nix
+++ modules/hardware/audio/pulseaudio/default.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
-  hardware.pulseaudio.enable = false;
-  hardware.pulseaudio.support32Bit = false;
-  hardware.pulseaudio.configFile = pkgs.runCommand "default.pa" {} ''
+  services.pulseaudio.enable = false;
+  services.pulseaudio.support32Bit = false;
+  services.pulseaudio.configFile = pkgs.runCommand "default.pa" {} ''
     sed 's/avoid-resampling$/avoid-resampling = true/' \
     ${pkgs.pulseaudio}/etc/pulse/default.pa > $out
   '';
