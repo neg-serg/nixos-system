5dc5fd937ab40ce8bd73030da6a46ac23bc3785d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-03-01 06:26:11 +0300

    [nix] more cache

--- modules/nix/settings.nix
+++ modules/nix/settings.nix
@@ -21,12 +21,20 @@
         "neg"
       ];
       substituters = [
+        "https://0uptime.cachix.org"
+        "https://cache.lix.systems"
         "https://ezkea.cachix.org"
+        "https://nix-community.cachix.org"
         "https://nix-gaming.cachix.org"
+        "https://nixpkgs-unfree.cachix.org"
       ];
       trusted-public-keys = [
-        "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
-        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+          "0uptime.cachix.org-1:ctw8yknBLg9cZBdqss+5krAem0sHYdISkw/IFdRbYdE="
+          "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+          "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+          "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
       ];
       connect-timeout = 5; # Bail early on missing cache hits (thx to nyx)
       cores = 32;
