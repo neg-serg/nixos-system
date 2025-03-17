d1f11cf0c74ecabddb98021d302edecdd27aba0a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-03-17 21:37:27 +0300

    [archives] more parallel archive stuff

--- modules/cli/archives/default.nix
+++ modules/cli/archives/default.nix
@@ -1,7 +1,12 @@
-{pkgs, ...}: {
+{pkgs, stable, ...}: {
   environment.systemPackages = with pkgs; [
+    lbzip2 # parallel bzip2
     p7zip # 7z x
+    pzip # parallel zip archiver
+    stable.rapidgzip # fast gzip unarchiver
     unrar-wrapper # unrar
     unzip # zip archive operations
+    xz # xz archiver
+    zip # zip archiver
   ];
 }
