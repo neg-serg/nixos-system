b4ea882feee781fcf5b5f3926e7bfac09bb3bb20
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-03-24 01:43:24 +0300

    [proxy] install hidify-app

--- modules/system/net/proxy.nix
+++ modules/system/net/proxy.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
+    hiddify-app # multi-platform proxy client
     nekoray # proxy manager
     v2raya # web-based proxy manager
     v2ray # cli proxy manager
