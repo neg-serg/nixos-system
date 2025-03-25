914e81eb0966c7a0c6e8e6d8c58711546c6ac8e5
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-03-25 19:01:08 +0300

    [system] add hw-probe

--- modules/system/pkgs.nix
+++ modules/system/pkgs.nix
@@ -2,6 +2,7 @@
   environment.systemPackages = with pkgs; [
     cryptsetup # stuff for LUKS
     dmidecode # extract system/memory/bios info
+    hw-probe # tool to get information about system
     kexec-tools # tools related to the kexec Linux feature
     lm_sensors # sensors
     pciutils # manipulate pci devices
