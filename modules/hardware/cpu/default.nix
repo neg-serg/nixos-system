a02c19f3bf8103e599def5e5d4511084c9d19354
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-03-25 18:52:43 +0300

    [cpu] add zenstates

---
+++ modules/hardware/cpu/default.nix
@@ -0,0 +1,5 @@
+{pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    zenstates # Linux utility for Ryzen processors and motherboards
+  ];
+}
--- modules/hardware/default.nix
+++ modules/hardware/default.nix
@@ -5,6 +5,7 @@
 }: {
   imports = [
     ./audio
+    ./cpu
     ./dygma # ergonimic keyboard brand
     ./io
     ./keyd # systemwide keyboard manager
