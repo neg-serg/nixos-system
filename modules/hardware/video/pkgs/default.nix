0f6f729b2989cc3ccbd0a3c557db8649862a72e1
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-03-25 18:56:26 +0300

    [video] update ddcontrol

--- modules/hardware/video/pkgs/default.nix
+++ modules/hardware/video/pkgs/default.nix
@@ -1,6 +1,6 @@
-{pkgs, stable, ...}: {
+{pkgs, ...}: {
   environment.systemPackages = with pkgs; [
-    stable.ddccontrol # ddc control
+    ddccontrol # ddc control
     ddcutil # rule monitor params
     edid-decode # edid decoder and tester
     read-edid # tool to read and parse edid from monitors
