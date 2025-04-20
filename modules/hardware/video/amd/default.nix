af2cabf570f58c81c2bcf21701e1d60b7c3ed544
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-04-20 17:29:33 +0300

    [amd] update xrandr settings

--- modules/hardware/video/amd/default.nix
+++ modules/hardware/video/amd/default.nix
@@ -13,16 +13,16 @@
   };
   services.xserver = {
     enable = true;
-    # xrandrHeads = [
-    #   {
-    #     output = "DisplayPort-1";
-    #     monitorConfig = ''
-    #       # Find these values in "$HOME/.local/share/xorg/Xorg.0.log They will be in this exact format so scroll until you find it.
-    #       Modeline  "3440x1440_175"  1019.75  3440 3488 3520 3600  1440 1443 1453 1619 +hsync -vsync
-    #       Option	"PreferredMode"	"3440x1440_175"
-    #     '';
-    #   }
-    # ];
+    xrandrHeads = [
+      {
+        output = "DisplayPort-1";
+        monitorConfig = ''
+          # Find these values in "$HOME/.local/share/xorg/Xorg.0.log They will be in this exact format so scroll until you find it.
+          Modeline  "3840x2160_240"  533.25  3840 3888 3920 4000  2160 2163 2168 2222 +hsync -vsync (133.3 kHz eP)
+          Option	"PreferredMode"	"3840x2160_240"
+        '';
+      }
+    ];
     monitorSection = ''
       Option      "StandbyTime" "0"
       Option      "SuspendTime" "0"
