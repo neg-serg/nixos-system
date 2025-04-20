9f3738ff1dec63b1cd3ac328145d854ed540dcbd
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-04-20 14:15:11 +0300

    [amd] remove resolution setup

--- modules/hardware/video/amd/default.nix
+++ modules/hardware/video/amd/default.nix
@@ -13,16 +13,16 @@
   };
   services.xserver = {
     enable = true;
-    xrandrHeads = [
-      {
-        output = "DisplayPort-1";
-        monitorConfig = ''
-          # Find these values in "$HOME/.local/share/xorg/Xorg.0.log They will be in this exact format so scroll until you find it.
-          Modeline  "3440x1440_175"  1019.75  3440 3488 3520 3600  1440 1443 1453 1619 +hsync -vsync
-          Option	"PreferredMode"	"3440x1440_175"
-        '';
-      }
-    ];
+    # xrandrHeads = [
+    #   {
+    #     output = "DisplayPort-1";
+    #     monitorConfig = ''
+    #       # Find these values in "$HOME/.local/share/xorg/Xorg.0.log They will be in this exact format so scroll until you find it.
+    #       Modeline  "3440x1440_175"  1019.75  3440 3488 3520 3600  1440 1443 1453 1619 +hsync -vsync
+    #       Option	"PreferredMode"	"3440x1440_175"
+    #     '';
+    #   }
+    # ];
     monitorSection = ''
       Option      "StandbyTime" "0"
       Option      "SuspendTime" "0"
