7f404452e7fd918c0551add0ce006ef47e02d915
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-12 04:47:34 +0300

    [flake] enable chaotic

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     nix-flatpak = { url = "github:gmodena/nix-flatpak"; }; # unstable branch. Use github:gmodena/nix-flatpak/?ref=<tag> to pin releases.
     lix-module = { url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz"; inputs.nixpkgs.follows = "nixpkgs"; };
     lanzaboote = { url = "github:nix-community/lanzaboote"; inputs.nixpkgs.follows = "nixpkgs"; };
-    # chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     # nixpkgs-oldstable.url = "github:NixOS/nixpkgs/nixos-23.11-small";
     # nixtheplanet.url = "github:matthewcroughan/NixThePlanet";
     # nixos-generators = {
@@ -31,7 +31,7 @@
     nixpkgs-master,
     nixpkgs-stable,
     nixpkgs-unstable,
-    # chaotic,
+    chaotic,
     # nixos-generators,
     # nixpkgs-oldstable,
     # nixtheplanet,
