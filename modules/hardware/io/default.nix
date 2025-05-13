1fd28a24d4e9e2d82a42d49ac2ec2968e6e7eb7d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-13 06:14:44 +0300

    [vial] use unstable

--- modules/hardware/io/default.nix
+++ modules/hardware/io/default.nix
@@ -1,4 +1,4 @@
-{pkgs, master, ...}: {
+{pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     blktrace # another disk test
     dmraid # Old-style RAID configuration utility
@@ -7,12 +7,12 @@
     fio # disk test
     gptfdisk # sgdisk
     ioping # io latency measuring tool
-    master.vial # add vial to configure keyboards
     mtools # utils for msdos disks
     multipath-tools # (kpartx) create loop devices for partitions in image
     nvme-cli # nvme manage tools
     ostree # git for os binaries
     parted # cli disk manager
     smartmontools # smartctl
+    vial # add vial to configure keyboards
   ];
 }
