7c147f21bf86b15dffb38e87ae7cb3aa14f5a40c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-13 08:46:24 +0300

    [environment] wayland support

--- modules/system/environment.nix
+++ modules/system/environment.nix
@@ -12,6 +12,7 @@
     # This is using a rec (recursive) expression to set and access XDG_BIN_HOME within the expression
     # For more on rec expressions see https://nix.dev/tutorials/first-steps/nix-language#recursive-attribute-set-rec
     sessionVariables = {
+      NIXOS_OZONE_WL = "1"; # Optional, hint Electron apps to use Wayland:
       XDG_CACHE_HOME = "$HOME/.cache";
       XDG_CONFIG_HOME = "$HOME/.config";
       XDG_DATA_HOME = "$HOME/.local/share";
