5dcaca7681bc339f9eebd086ea94ad75c76fee5b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-13 08:46:39 +0300

    [session] hyprland initial commit

--- modules/user/session/default.nix
+++ modules/user/session/default.nix
@@ -1,10 +1,16 @@
-{pkgs, ...}: {
-  imports = [ ./wayland ];
+{pkgs, inputs, ...}: {
   environment.systemPackages = with pkgs; [
     xorg.xdpyinfo # display info
     xorg.xhost # install xhost to setup X11 ACL
   ];
   programs.kdeconnect.enable = true;
+  programs.hyprland = {
+    enable = true;
+    # set the flake package
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # make sure to also set the portal package, so that they are in sync
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  };
   services = {
     accounts-daemon.enable = true; # AccountsService a DBus service for accessing the list of user accounts and info…
     dbus.implementation = "broker";
