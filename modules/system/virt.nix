a2dde371e09b67559a3589915e8b29598bb4831c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-13 06:11:49 +0300

    [virt] use latest

--- modules/system/virt.nix
+++ modules/system/virt.nix
@@ -8,10 +8,9 @@
     };
   };
   environment.systemPackages = with pkgs; [
-    dxvk # for plugins compatibility
-    quickemu # fast and simple vm builder
-    wineWowPackages.unstable # tool to run windows packages
     dxvk # setup script for dxvk
+    quickemu # fast and simple vm builder
     winetricks # stuff to install dxvk
+    wineWowPackages.unstable # tool to run windows packages
   ];
 }
