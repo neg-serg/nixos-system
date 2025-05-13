daffc6b269cbf9ec04fa804f6556bf16af64b25b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-13 06:09:25 +0300

    [dsp] use latest

--- modules/hardware/audio/dsp/default.nix
+++ modules/hardware/audio/dsp/default.nix
@@ -1,4 +1,4 @@
-{pkgs, stable, ...}:
+{pkgs, ...}:
 with {
   camilladsp = pkgs.callPackage ../../../../packages/camilladsp {};
 }; {
@@ -6,9 +6,9 @@ with {
     brutefir # one of the best FIR filters for linux
     camilladsp # one of the best DSP for linux
     carla # audio plugin host  TODO: install gui later (github.com/HEnquist/camilladsp)
-    stable.jamesdsp # pipewire dsp
+    jamesdsp # pipewire dsp
     lsp-plugins # various linux dsp
     yabridgectl # vst control for linux
-    stable.yabridge # vst bridge for linux
+    yabridge # vst bridge for linux
   ];
 }
