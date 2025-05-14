2c64f09fdfae79d1a2c78e5facc8823253a85fff
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-14 12:25:19 +0300

    [pipewire] wireplumber: disable verbose logging

--- modules/hardware/audio/pipewire/default.nix
+++ modules/hardware/audio/pipewire/default.nix
@@ -9,10 +9,10 @@
     wireplumber = {
       package = pkgs.wireplumber;
       extraConfig = {
-        # Tell wireplumber to be more verbose
-        "10-log-level-debug" = {
-          "context.properties"."log.level" = "D"; # output debug logs
-        };
+        # # Tell wireplumber to be more verbose
+        # "10-log-level-debug" = {
+        #   "context.properties"."log.level" = "D"; # output debug logs
+        # };
         # Default volume is by default set to 0.4 instead set it to 1.0
         "10-default-volume" = {
           "wireplumber.settings"."device.routes.default-sink-volume" = 1.0;
