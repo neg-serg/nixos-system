167fb1cceac24539bc1474cc943aad9c4ac5fca2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-15 07:04:07 +0300

    [fonts] cleanup

--- modules/user/fonts.nix
+++ modules/user/fonts.nix
@@ -2,8 +2,6 @@
   fonts.fontDir.enable = true; # add fontdir support for nixos
   fonts.packages = with pkgs; [
     liberation_ttf # Liberation Fonts, replacements for Times New Roman, Arial, and Courier New
-    nerd-fonts.fira-code # firacode nerdfont
-    proggyfonts # yet another fonts for coding
     symbola # Basic Latin, Greek, Cyrillic and many Symbol blocks of Unicode
     texlivePackages.opensans # The Open Sans font family, and LaTeX support
   ];
