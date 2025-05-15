3db477174ba4c890b9b33a70d1f382e204d701d4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-15 07:47:25 +0300

    [cpu] disable zenstates

--- modules/hardware/cpu/default.nix
+++ modules/hardware/cpu/default.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
-    zenstates # Linux utility for Ryzen processors and motherboards
+    # zenstates # Linux utility for Ryzen processors and motherboards
   ];
 }
