59d52e253fc463d9ba8f86b198906a42e60198a4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-17 15:36:32 +0300

    [xdg] portal: fix

--- modules/user/xdg.nix
+++ modules/user/xdg.nix
@@ -1,6 +1,16 @@
-{...}: {
+{inputs, pkgs, ...}: {
   xdg.portal = {
     enable = true;
     xdgOpenUsePortal = true;
+    extraPortals = [
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland
+      pkgs.xdg-desktop-portal-termfilechooser
+    ];
+    config = {
+      common.default = [ "hyprland" ];
+      common."org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" ];
+      hyprland.default = [ "hyprland" ];
+      hyprland."org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" ];
+    };
   };
 }
