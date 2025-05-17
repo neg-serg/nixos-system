3d132b0efcdc0b8dd3a2177d6897d0478eba3786
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-17 12:52:07 +0300

    [flake] lix update

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nix-gaming = { url = "github:fufexan/nix-gaming"; inputs.nixpkgs.follows = "nixpkgs"; };
     nixpkgs.url = "github:NixOS/nixpkgs";
     nix-flatpak = { url = "github:gmodena/nix-flatpak"; }; # unstable branch. Use github:gmodena/nix-flatpak/?ref=<tag> to pin releases.
-    lix-module = { url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz"; inputs.nixpkgs.follows = "nixpkgs"; };
+    lix-module = { url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.0.tar.gz"; inputs.nixpkgs.follows = "nixpkgs"; };
     lanzaboote = { url = "github:nix-community/lanzaboote"; inputs.nixpkgs.follows = "nixpkgs"; };
     chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     hyprland = { url = "github:hyprwm/Hyprland"; };
