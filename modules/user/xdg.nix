2d713e8282002f9c1c95f5079b4dd42f88062ccd
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-18 05:18:29 +0300

    [xdg] add gtk portal

--- modules/user/xdg.nix
+++ modules/user/xdg.nix
@@ -5,11 +5,12 @@
     extraPortals = [
       inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland
       pkgs.xdg-desktop-portal-termfilechooser
+      pkgs.xdg-desktop-portal-gtk
     ];
     config = {
-      common.default = [ "hyprland" ];
+      common.default = [ "hyprland" "gtk" ];
       common."org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" ];
-      hyprland.default = [ "hyprland" ];
+      hyprland.default = [ "hyprland" "gtk" ];
       hyprland."org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" ];
     };
   };
