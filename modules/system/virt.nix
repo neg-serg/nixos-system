e2de94b41ffce74e4d8c354fde71be226f14067c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-05-23 12:39:54 +0300

    [virt] add more guest stuff

--- modules/system/virt.nix
+++ modules/system/virt.nix
@@ -24,9 +24,13 @@
     };
   };
 
+  services.spice-webdavd.enable = true;
+  virtualisation.libvirtd.qemu.vhostUserPackages = [ pkgs.virtiofsd ];
+
   environment.systemPackages = with pkgs; [
     dxvk # setup script for dxvk
     quickemu # fast and simple vm builder
+    guestfs-tools # virt-sysprep to prepare image for use
     winetricks # stuff to install dxvk
     wineWowPackages.unstable # tool to run windows packages
   ];
