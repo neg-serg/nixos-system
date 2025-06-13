389eb1c3daa7720f727ac82b954115a852b89741
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-13 16:05:57 +0300

    [amd] remove xserver section

--- modules/hardware/video/amd/default.nix
+++ modules/hardware/video/amd/default.nix
@@ -12,28 +12,4 @@
       vulkan-validation-layers
     ];
   };
-  services.xserver = {
-    enable = false;
-    xrandrHeads = [
-      {
-        output = "DisplayPort-1";
-        monitorConfig = ''
-          # Find these values in "$HOME/.local/share/xorg/Xorg.0.log They will be in this exact format so scroll until you find it.
-          Modeline "3840x2160_240"  2315.95  3840 3936 3968 4096  2160 2166 2176 2356 +hsync -vsync
-          Option	"PreferredMode"	"3840x2160_240"
-        '';
-      }
-    ];
-    monitorSection = ''
-      Option      "StandbyTime"     "0"
-      Option      "SuspendTime"     "0"
-      Option      "OffTime"         "0"
-      Option      "BlankTime"       "0"
-    '';
-    deviceSection = ''
-      Option "VariableRefresh"      "false"
-      Option "EnablePageFlip"       "off"
-    '';
-  };
-  services.xserver.videoDrivers = ["amdgpu"];
 }
