886e51c27c05a9dc2d3b7bc8a7a1cd0371b9eaa9
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-25 22:24:42 +0300

    [avahi] better settings

--- modules/servers/avahi/default.nix
+++ modules/servers/avahi/default.nix
@@ -2,8 +2,12 @@
   services.avahi = {
     enable = true;
     nssmdns4 = true; # ^^ Needed to allow samba to automatically register mDNS records (without the need for an `extraServiceFile`
+    nssmdns6 = true; # Enable mDNS for IPv6
     openFirewall = true;
-    publish.enable = true;
-    publish.userServices = true;
+    publish = {
+      enable = true;
+      userServices = true;
+      workstation = true;
+    };
   };
 }
