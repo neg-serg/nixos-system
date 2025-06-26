b050c958ea5cf713fd80f0179b8a6316a533a507
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-06-26 20:03:35 +0300

    [carla] fix

--- modules/hardware/audio/dsp/default.nix
+++ modules/hardware/audio/dsp/default.nix
@@ -2,10 +2,15 @@
 with {
   camilladsp = pkgs.callPackage ../../../../packages/camilladsp {};
 }; {
+  nixpkgs.overlays = [
+    (self: super: {
+      # audio plugin host  TODO: install gui later (github.com/HEnquist/camilladsp)
+      carla = super.carla.override { python3 = super.python312; }; 
+    })
+  ];
   environment.systemPackages = with pkgs; [
     brutefir # one of the best FIR filters for linux
     camilladsp # one of the best DSP for linux
-    carla # audio plugin host  TODO: install gui later (github.com/HEnquist/camilladsp)
     jamesdsp # pipewire dsp
     lsp-plugins # various linux dsp
     yabridgectl # vst control for linux
