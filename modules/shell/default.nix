c6305143ee06ac15965ccd0f33cd2e7255d76de2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-07-11 19:33:59 +0300

    [shell] more systemwide shells

--- modules/shell/default.nix
+++ modules/shell/default.nix
@@ -2,7 +2,10 @@
   programs.zsh = {enable = true;};
   environment.systemPackages = with pkgs; [
     bash-completion # generic bash completions
+    dash # faster sh
     nix-bash-completions # nix-related bash-completions
     nix-zsh-completions # nix-related zsh-completion
+    nushell # modern shell written in Rust
+    oils-for-unix # better bash
   ];
 }
