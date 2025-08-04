3196fb052100e76777f89517475daf03f7f39eb9
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-04 03:28:58 +0300

    [shell] carapace install

--- modules/shell/default.nix
+++ modules/shell/default.nix
@@ -2,6 +2,7 @@
   programs.zsh = {enable = true;};
   environment.systemPackages = with pkgs; [
     bash-completion # generic bash completions
+    carapace # universal autocompletion
     dash # faster sh
     nix-bash-completions # nix-related bash-completions
     nix-zsh-completions # nix-related zsh-completion
