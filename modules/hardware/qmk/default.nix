caffe1dbf3cb24a93e0313800ac487c4b4115c5a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-14 19:25:45 +0300

    [qmk] stash

--- modules/hardware/qmk/default.nix
+++ modules/hardware/qmk/default.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
     keymapviz # qmk keymap.c visualizer
-    qmk_hid # commandline tool for interactng with QMK devices over HID 
+    qmk_hid # commandline tool for interactng with QMK devices over HID
     qmk # program to help users work with QMK Firmware
     qmk-udev-rules # add qmk udev rules
   ];
