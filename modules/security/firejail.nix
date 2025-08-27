512dfffa0b113f04de01453aad0959e193558c25
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-27 05:02:15 +0300

    [firejail] enable

--- modules/security/firejail.nix
+++ modules/security/firejail.nix
@@ -4,12 +4,8 @@
   ...
 }: {
   programs.firejail = {
-    enable = false;
+    enable = true;
     wrappedBinaries = {
-      firefox = {
-        executable = "${lib.getBin pkgs.firefox}/bin/firefox";
-        profile = "${pkgs.firejail}/etc/firejail/firefox.profile";
-      };
       mpv = {
         executable = "${lib.getBin pkgs.mpv}/bin/mpv";
         profile = "${pkgs.firejail}/etc/firejail/mpv.profile";
