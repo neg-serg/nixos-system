326ea74a3c0fd0a3ad388b9256baf5299354ce51
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-28 01:35:57 +0300

    [llm] better ollama settings

--- modules/llm/default.nix
+++ modules/llm/default.nix
@@ -6,5 +6,17 @@
   services.ollama = {
     enable = true;
     acceleration = "rocm";
+    models = "/zero/llm/ollama-models";
+  };
+  systemd.tmpfiles.rules = [
+    "d /zero/llm 0750 ollama ollama -"
+    "d /zero/llm/ollama-models 0750 ollama ollama -"
+  ];
+  users.groups.ollama = {};
+  users.users.ollama = {
+    isSystemUser = true;
+    group = "ollama";
+    home = "/zero/llm/ollama-models";
+    createHome = true;
   };
 }
