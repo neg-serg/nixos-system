f7ca2610ac0a9a08404e31d27024fcccf98f9c59
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-29 08:43:19 +0300

    [unbound] add

---
+++ modules/servers/unbound/default.nix
@@ -0,0 +1,12 @@
+{...}: {
+  services.unbound = {
+    enable = true;
+    settings = {
+      server.interface = [ "127.0.0.1" ];
+      server.port = 5353;
+      server.do-tcp = "yes";
+      server.do-udp = "yes";
+      server.verbosity = 1;
+    };
+  };
+}
