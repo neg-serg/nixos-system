73725a903a2e97de641ab2871bd2fded4698cf6b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-29 08:43:36 +0300

    [adguardhome] go to local dns

--- modules/servers/adguardhome/default.nix
+++ modules/servers/adguardhome/default.nix
@@ -1,4 +1,12 @@
 {...}: {
-  services.adguardhome.enable = true;
-  services.adguardhome.openFirewall = true;
+  services.adguardhome = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      dns = {
+        upstream_dns = [ "127.0.0.1:5353" ];
+        bootstrap_dns = [ "1.1.1.1" "8.8.8.8" ];
+      };
+    };
+  };
 }
