c2749786436a8081636650e135b535684f3969bb
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-29 06:20:47 +0300

    [net] stash new config

--- modules/system/net/default.nix
+++ modules/system/net/default.nix
@@ -49,10 +49,14 @@
     networks."10-lan" = {
       matchConfig.Name = "net0";
       networkConfig.DHCP = "ipv4";
+      dhcpV4Config.UseDNS = true; # use DNS from MikroTik
+      dhcpV4Config.UseRoutes = true;  # apply default route from MikroTik
     };
     networks."11-lan" = {
       matchConfig.Name = "net1";
       networkConfig.DHCP = "ipv4";
+      dhcpV4Config.UseDNS = true; # use DNS from MikroTik
+      dhcpV4Config.UseRoutes = true;  # apply default route from MikroTik
     };
     networks."10-br0" = {
       matchConfig.Name = "br0";
