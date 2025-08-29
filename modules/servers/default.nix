8995c7b0bb2fcec32b5705b76397cf13e4bbd58d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-29 08:49:13 +0300

    [unbound] startup

--- modules/servers/default.nix
+++ modules/servers/default.nix
@@ -7,6 +7,7 @@
     ./nextcloud
     ./openssh
     ./syncthing
+    ./unbound
     ./wakapi
   ];
 }
--- modules/servers/unbound/default.nix
+++ modules/servers/unbound/default.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       server.interface = [ "127.0.0.1" ];
-      server.port = 5353;
+      server.port = 53;
       server.do-tcp = "yes";
       server.do-udp = "yes";
       server.verbosity = 1;
