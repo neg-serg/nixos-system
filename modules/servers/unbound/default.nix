92640e8a03391b94a44b39bc5eb5f59ff81a7566
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-31 01:50:42 +0300

    [unbound] refactoring

--- modules/servers/unbound/default.nix
+++ modules/servers/unbound/default.nix
@@ -1,12 +1,14 @@
-{...}: {
+_: {
   services.unbound = {
     enable = true;
     settings = {
-      server.interface = ["127.0.0.1"];
-      server.port = 5353;
-      server.do-tcp = "yes";
-      server.do-udp = "yes";
-      server.verbosity = 1;
+      server = {
+        interface = ["127.0.0.1"];
+        port = 5353;
+        "do-tcp" = "yes";
+        "do-udp" = "yes";
+        verbosity = 1;
+      };
     };
   };
 }
