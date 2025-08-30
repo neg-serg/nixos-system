45e9ed8e2a08e6a4ad9f6b13549c3edc7f53c5e8
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-30 23:25:58 +0300

    [cachix] new cache

---
+++ cachix/hercules-ci.nix
@@ -0,0 +1,13 @@
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://hercules-ci.cachix.org"
+      ];
+      trusted-public-keys = [
+        "hercules-ci.cachix.org-1:ZZeDl9Va+xe9j+KqdzoBZMFJHVQ42Uu/c/1/KMC5Lw0="
+        
+      ];
+    };
+  };
+}
