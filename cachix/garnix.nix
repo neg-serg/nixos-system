7f73e6784f2ad5e2a1b789a65a86d1aee19ea68c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-30 23:57:58 +0300

    [cachix] add garnix

---
+++ cachix/garnix.nix
@@ -0,0 +1,13 @@
+{
+  nix = {
+    settings = {
+      substituters = [
+        "https://cache.garnix.io"
+      ];
+      trusted-public-keys = [
+        "cache.garnix.io:CTFPyK8EEhZ3jAC5vVsQt1zArhcXd1LSeX776BFqe7A="
+      ];
+    };
+  };
+}
+
