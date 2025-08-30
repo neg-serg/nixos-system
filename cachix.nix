484a20631b161bf7fa8da0d421aa175ff7380a07
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-31 01:52:56 +0300

    [cachix] fix warning

--- cachix.nix
+++ cachix.nix
@@ -1,7 +1,7 @@
 # WARN: this file will get overwritten by $ cachix use <name>
 {lib, ...}: let
   folder = ./cachix;
-  toImport = name: value: folder + ("/" + name);
+  toImport = name: _value: folder + ("/" + name);
   filterCaches = key: value: value == "regular" && lib.hasSuffix ".nix" key;
   imports = lib.mapAttrsToList toImport (lib.filterAttrs filterCaches (builtins.readDir folder));
 in {
