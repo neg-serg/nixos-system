61d44ce6f08353279e4c98ca550d84d884af94f6
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-31 07:26:27 +0300

    [servers/mpd] add feature flag; gate with mkIf servicesProfiles.mpd.enable

--- modules/servers/mpd/default.nix
+++ modules/servers/mpd/default.nix
@@ -1,66 +1,72 @@
-_: let
+{ lib, config, ... }:
+let
+  cfg = config.servicesProfiles.mpd;
   myUser = "neg";
   myHome = "/home/${myUser}";
 in {
-  systemd.services.mpd.serviceConfig = {
-    Environment = "XDG_RUNTIME_DIR=/run/user/1000";
-  };
+  options.servicesProfiles.mpd.enable = lib.mkEnableOption "MPD (Music Player Daemon) profile";
 
-  services.mpd = {
-    enable = true;
-    user = myUser;
-    group = myUser;
-
-    startWhenNeeded = false; # important
-    dataDir = "${myHome}/.config/mpd";
-    musicDirectory = "${myHome}/music";
-    network = {
-      listenAddress = "any";
-      port = 6600;
+  config = lib.mkIf cfg.enable {
+    systemd.services.mpd.serviceConfig = {
+      Environment = "XDG_RUNTIME_DIR=/run/user/1000";
     };
 
-    extraConfig = ''
-      log_file "/dev/null"
-      max_output_buffer_size "131072"
-      max_connections "100"
-      connection_timeout "864000"
-      restore_paused "yes"
-      save_absolute_paths_in_playlists "yes"
-      follow_inside_symlinks "yes"
-      replaygain "off"
-      auto_update "yes"
-      mixer_type "hardware"
+    services.mpd = {
+      enable = true;
+      user = myUser;
+      group = myUser;
 
-      audio_output {
-        type "alsa"
-        name "PipeWire"
-        device "default"
-        auto_resample "no"
-        auto_format "no"
-        auto_channels "no"
-        replay_gain_handler "none"
-        dsd_native "yes"
-        dop "no"
-        tags "yes"
-      }
+      startWhenNeeded = false; # important
+      dataDir = "${myHome}/.config/mpd";
+      musicDirectory = "${myHome}/music";
+      network = {
+        listenAddress = "any";
+        port = 6600;
+      };
 
-      audio_output {
-        type "alsa"
-        name "RME ADI-2/4 PRO SE"
-        device "hw:CARD=SE53011083"
-        auto_resample "no"
-        auto_format "no"
-        auto_channels "no"
-        replay_gain_handler "none"
-        dsd_native "yes"
-        dop "no"
-        tags "yes"
-      }
-    '';
-  };
+      extraConfig = ''
+        log_file "/dev/null"
+        max_output_buffer_size "131072"
+        max_connections "100"
+        connection_timeout "864000"
+        restore_paused "yes"
+        save_absolute_paths_in_playlists "yes"
+        follow_inside_symlinks "yes"
+        replaygain "off"
+        auto_update "yes"
+        mixer_type "hardware"
 
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [6600];
+        audio_output {
+          type "alsa"
+          name "PipeWire"
+          device "default"
+          auto_resample "no"
+          auto_format "no"
+          auto_channels "no"
+          replay_gain_handler "none"
+          dsd_native "yes"
+          dop "no"
+          tags "yes"
+        }
+
+        audio_output {
+          type "alsa"
+          name "RME ADI-2/4 PRO SE"
+          device "hw:CARD=SE53011083"
+          auto_resample "no"
+          auto_format "no"
+          auto_channels "no"
+          replay_gain_handler "none"
+          dsd_native "yes"
+          dop "no"
+          tags "yes"
+        }
+      '';
+    };
+
+    networking.firewall = {
+      enable = true;
+      allowedTCPPorts = [6600];
+    };
   };
 }
