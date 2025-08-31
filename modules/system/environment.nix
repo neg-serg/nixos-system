eec9eb1ee76aa82503399aa46ca9ba452da0a716
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-31 06:51:03 +0300

    [env] remove LD_LIBRARY_PATH override to resolve conflict with upstream shells-environment

--- modules/system/environment.nix
+++ modules/system/environment.nix
@@ -19,8 +19,6 @@
       # Prefer Mesa VAAPI/VDPAU on AMD (radeonsi)
       LIBVA_DRIVER_NAME = "radeonsi";
       VDPAU_DRIVER = "radeonsi";
-      # Legacy GL apps; extend any base value
-      LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:/run/opengl-driver/lib:/run/opengl-driver-32/lib";
       XDG_CACHE_HOME = "$HOME/.cache";
       XDG_CONFIG_HOME = "$HOME/.config";
       XDG_DATA_HOME = "$HOME/.local/share";
