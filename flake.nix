8bcd307965d47e880cdfed8e88df9d811d2c59d2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-31 04:13:04 +0300

    [flake] this is more correct using of cache

--- flake.nix
+++ flake.nix
@@ -34,6 +34,43 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  # Make Cachix caches available to all `nix {build,develop,run}` commands
+  nixConfig = {
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://0uptime.cachix.org"
+      "https://chaotic-nyx.cachix.org"
+      "https://cuda-maintainers.cachix.org"
+      "https://devenv.cachix.org"
+      "https://ezkea.cachix.org"
+      "https://cache.garnix.io"
+      "https://hercules-ci.cachix.org"
+      "https://hyprland.cachix.org"
+      "https://neg-serg.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://nix-gaming.cachix.org"
+      "https://nixpkgs-unfree.cachix.org"
+      "https://nixpkgs-wayland.cachix.org"
+      "https://numtide.cachix.org"
+    ];
+    trusted-public-keys = [
+      "0uptime.cachix.org-1:ctw8yknBLg9cZBdqss+5krAem0sHYdISkw/IFdRbYdE="
+      "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "ezkea.cachix.org-1:ioBmUbJTZIKsHmWWXPe1FSFbeVe+afhfgqgTSNd34eI="
+      "cache.garnix.io:CTFPyK8EEhZ3jAC5vVsQt1zArhcXd1LSeX776BFqe7A="
+      "hercules-ci.cachix.org-1:ZZeDl9Va+xe9j+KqdzoBZMFJHVQ42Uu/c/1/KMC5Lw0="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "neg-serg.cachix.org-1:MZ+xYOrDj1Uhq8GTJAg//KrS4fAPpnIvaWU/w3Qz/wo="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+    ];
+  };
   outputs = inputs @ {
     self,
     chaotic,
@@ -97,7 +134,6 @@
           };
           modules = [
             ./init.nix
-            ./cachix.nix
             nix-flatpak.nixosModules.nix-flatpak
             lanzaboote.nixosModules.lanzaboote
             chaotic.nixosModules.default
