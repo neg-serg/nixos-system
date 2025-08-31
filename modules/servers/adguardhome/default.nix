1d371493ffc55f5e0ac85c05d98f80a4da780768
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-31 07:24:51 +0300

    [servers/adguardhome] add feature flag; gate with mkIf servicesProfiles.adguardhome.enable

--- modules/servers/adguardhome/default.nix
+++ modules/servers/adguardhome/default.nix
@@ -1,16 +1,23 @@
-_: {
-  services.adguardhome = {
-    enable = true;
-    openFirewall = true;
-    settings = {
-      dns = {
-        upstream_dns = ["127.0.0.1:5353"];
-        bootstrap_dns = ["1.1.1.1" "8.8.8.8"];
-        # Local rewrites so LAN clients can resolve Nextcloud host
-        rewrites = [
-          { domain = "telfir"; answer = "192.168.2.240"; }
-          { domain = "telfir.local"; answer = "192.168.2.240"; }
-        ];
+{ lib, config, ... }:
+let
+  cfg = config.servicesProfiles.adguardhome;
+in {
+  options.servicesProfiles.adguardhome.enable = lib.mkEnableOption "AdGuard Home profile";
+
+  config = lib.mkIf cfg.enable {
+    services.adguardhome = {
+      enable = true;
+      openFirewall = true;
+      settings = {
+        dns = {
+          upstream_dns = ["127.0.0.1:5353"];
+          bootstrap_dns = ["1.1.1.1" "8.8.8.8"];
+          # Local rewrites so LAN clients can resolve Nextcloud host
+          rewrites = [
+            { domain = "telfir"; answer = "192.168.2.240"; }
+            { domain = "telfir.local"; answer = "192.168.2.240"; }
+          ];
+        };
       };
     };
   };
