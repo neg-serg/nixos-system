131aec3b025a1ba7856b4aa836d92dd6e58daa0e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-31 15:58:27 +0300

    [games] Enable GameMode (renice -10, softrealtime); enable MangoHud; add Proton-GE; enable steam-hardware

--- modules/user/games/default.nix
+++ modules/user/games/default.nix
@@ -6,6 +6,8 @@
       gamescopeSession.enable = true;
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+      # Add Proton-GE for better compatibility/perf in some titles
+      extraCompatPackages = [pkgs.proton-ge-custom];
     };
 
     gamescope = {
@@ -14,15 +16,19 @@
     };
 
     gamemode = {
-      enable = false;
+      enable = true;
       enableRenice = true;
       settings = {
         general = {
-          softrealtime = "auto";
-          renice = 15;
+          softrealtime = "on";
+          # Negative values increase priority; -10 is a safe bump
+          renice = -10;
         };
       };
     };
+
+    # On-screen performance HUD (toggle via MANGOHUD=1)
+    mangohud.enable = true;
   };
 
   environment.systemPackages = with pkgs; [protontricks];
@@ -33,4 +39,7 @@
     source = "${pkgs.gamemode}/bin/gamemoderun";
     capabilities = "cap_sys_ptrace,cap_sys_nice+pie";
   };
+
+  # Expose udev rules/devices used by various game controllers/VR etc
+  hardware.steam-hardware.enable = true;
 }
