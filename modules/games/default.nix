c2a68f8cacd28754f37d7bcda80d16fff1a4f210
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-31 15:59:27 +0300

    [games] sysctl: set vm.swappiness=10 for heavy gaming workloads

--- modules/games/default.nix
+++ modules/games/default.nix
@@ -4,6 +4,7 @@ _: {
     "kernel.sched_cfs_bandwidth_slice_us" = 3000; # better settings for cfs
     "net.ipv4.tcp_fin_timeout" = 5; # decrease default fin timeout
     "vm.max_map_count" = 16777216; # need for some games
+    "vm.swappiness" = 10; # avoid swapping under load on 64GB RAM
   };
   # environment.systemPackages = with pkgs; [
   #   (inputs.nix-gaming.packages.${hostPlatform.system}.star-citizen.override {
