5e9b67deeb4233e36d9845d2de4ddd47f110359a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-31 07:27:36 +0300

    [servers/syncthing] add feature flag; gate with mkIf servicesProfiles.syncthing.enable

--- modules/servers/syncthing/default.nix
+++ modules/servers/syncthing/default.nix
@@ -4,57 +4,62 @@
   ...
 }: let
   hasSynSecret = builtins.pathExists (../../.. + "/secrets/syncthing.sops.yaml");
+  cfg = config.servicesProfiles.syncthing;
 in {
-  # Register secret only if present to keep evaluation robust without secrets
-  sops.secrets."syncthing/gui-pass" = lib.mkIf hasSynSecret {
-    sopsFile = ../../../secrets/syncthing.sops.yaml;
-  };
+  options.servicesProfiles.syncthing.enable = lib.mkEnableOption "Syncthing profile";
 
-  services.syncthing = {
-    enable = true;
-    user = "neg";
-    settings.gui = {
+  config = lib.mkIf cfg.enable {
+    # Register secret only if present to keep evaluation robust without secrets
+    sops.secrets."syncthing/gui-pass" = lib.mkIf hasSynSecret {
+      sopsFile = ../../../secrets/syncthing.sops.yaml;
+    };
+
+    services.syncthing = {
+      enable = true;
       user = "neg";
-      # Password is managed out-of-band (either already configured in Syncthing
-      # or updated manually via GUI/API). Avoid reading secrets at eval time.
-    };
-    dataDir = "/zero/syncthing/data";
-    configDir = "/zero/syncthing/config";
-    overrideDevices = true; # overrides any devices added or deleted through the WebUI
-    overrideFolders = true; # overrides any folders added or deleted through the WebUI
-    settings = {
-      devices = {
-        "telfir" = {id = "EZG57BT-TANWJ2R-QDVLV5X-4DKP7GU-HQENUT7-MA43GUU-AV3IN6P-7KKGZA3";};
-        "Pixel 7 Pro" = {id = "OWGOTRT-Q4LV2MR-QLVIFZH-LPWZ4DP-TANYCAM-SXC2W2A-BL4VSHS-KWXLVAB";};
-        "DX180" = {id = "NKSYBIH-G5BV2FK-ZHHL27B-MWZT3OJ-DPTF7TH-O6HE5CM-3CARZ5K-6CIUSQI";};
-        "OPPO X7 Ultra" = {id = "JHDQEDC-YN67IMD-B7WFZTI-Y4CPKMY-MUPRBYK-OAFOMPC-IJVDVOV-AOBILAX";};
+      settings.gui = {
+        user = "neg";
+        # Password is managed out-of-band (either already configured in Syncthing
+        # or updated manually via GUI/API). Avoid reading secrets at eval time.
       };
-      folders = {
-        "music-upload" = {
-          # Name of folder in Syncthing, also the folder ID
-          path = "/zero/syncthing/music-upload"; # Which folder to add to Syncthing
-          devices = [
-            "telfir"
-            "Pixel 7 Pro"
-            "DX180"
-            "OPPO X7 Ultra"
-          ]; # Which devices to share the folder with
+      dataDir = "/zero/syncthing/data";
+      configDir = "/zero/syncthing/config";
+      overrideDevices = true; # overrides any devices added or deleted through the WebUI
+      overrideFolders = true; # overrides any folders added or deleted through the WebUI
+      settings = {
+        devices = {
+          "telfir" = {id = "EZG57BT-TANWJ2R-QDVLV5X-4DKP7GU-HQENUT7-MA43GUU-AV3IN6P-7KKGZA3";};
+          "Pixel 7 Pro" = {id = "OWGOTRT-Q4LV2MR-QLVIFZH-LPWZ4DP-TANYCAM-SXC2W2A-BL4VSHS-KWXLVAB";};
+          "DX180" = {id = "NKSYBIH-G5BV2FK-ZHHL27B-MWZT3OJ-DPTF7TH-O6HE5CM-3CARZ5K-6CIUSQI";};
+          "OPPO X7 Ultra" = {id = "JHDQEDC-YN67IMD-B7WFZTI-Y4CPKMY-MUPRBYK-OAFOMPC-IJVDVOV-AOBILAX";};
         };
-        "picture-upload" = {
-          # Name of folder in Syncthing, also the folder ID
-          path = "/zero/syncthing/picture-upload"; # Which folder to add to Syncthing
-          devices = [
-            "Pixel 7 Pro"
-            "DX180"
-          ]; # Which devices to share the folder with
+        folders = {
+          "music-upload" = {
+            # Name of folder in Syncthing, also the folder ID
+            path = "/zero/syncthing/music-upload"; # Which folder to add to Syncthing
+            devices = [
+              "telfir"
+              "Pixel 7 Pro"
+              "DX180"
+              "OPPO X7 Ultra"
+            ]; # Which devices to share the folder with
+          };
+          "picture-upload" = {
+            # Name of folder in Syncthing, also the folder ID
+            path = "/zero/syncthing/picture-upload"; # Which folder to add to Syncthing
+            devices = [
+              "Pixel 7 Pro"
+              "DX180"
+            ]; # Which devices to share the folder with
+          };
         };
       };
     };
+    # Syncthing ports: 8384 for remote access to GUI
+    # 22000 TCP and/or UDP for sync traffic
+    # 21027/UDP for discovery
+    # source: https://docs.syncthing.net/users/firewall.html
+    networking.firewall.allowedTCPPorts = [8384 22000];
+    networking.firewall.allowedUDPPorts = [22000 21027];
   };
-  # Syncthing ports: 8384 for remote access to GUI
-  # 22000 TCP and/or UDP for sync traffic
-  # 21027/UDP for discovery
-  # source: https://docs.syncthing.net/users/firewall.html
-  networking.firewall.allowedTCPPorts = [8384 22000];
-  networking.firewall.allowedUDPPorts = [22000 21027];
 }
