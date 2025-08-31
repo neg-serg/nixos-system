5bf64b9a2bcb22b2ccd53c5c60824608773db261
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-31 07:28:09 +0300

    [servers/nextcloud] add feature flag; gate with mkIf servicesProfiles.nextcloud.enable

--- modules/servers/nextcloud/default.nix
+++ modules/servers/nextcloud/default.nix
@@ -5,29 +5,35 @@
   ...
 }: let
   hasNcSecret = builtins.pathExists (../../.. + "/secrets/nextcloud.sops.yaml");
+  cfg = config.servicesProfiles.nextcloud;
 in {
   # Optional: nginx reverse proxy + ACME integration (guarded by an enable flag)
   imports = [ ./nginx.nix ./caddy.nix ];
-  # Register SOPS secret only if the file exists to avoid eval errors
-  sops.secrets."nextcloud/admin-pass" = lib.mkIf hasNcSecret {
-    sopsFile = ../../../secrets/nextcloud.sops.yaml;
-  };
 
-  services.nextcloud = {
-    enable = true;
-    package = pkgs.nextcloud31;
-    # Default to localhost; override to a FQDN when exposing externally
-    hostName = lib.mkDefault "localhost";
-    database.createLocally = true;
-    configureRedis = true;
-    datadir = "/nextcloud";
-    config =
-      {
-        adminuser = "init";
-        dbtype = "mysql";
-      }
-      // (lib.optionalAttrs hasNcSecret {
-        adminpassFile = config.sops.secrets."nextcloud/admin-pass".path;
-      });
+  options.servicesProfiles.nextcloud.enable = lib.mkEnableOption "Nextcloud server profile";
+
+  config = lib.mkIf cfg.enable {
+    # Register SOPS secret only if the file exists to avoid eval errors
+    sops.secrets."nextcloud/admin-pass" = lib.mkIf hasNcSecret {
+      sopsFile = ../../../secrets/nextcloud.sops.yaml;
+    };
+
+    services.nextcloud = {
+      enable = true;
+      package = pkgs.nextcloud31;
+      # Default to localhost; override to a FQDN when exposing externally
+      hostName = lib.mkDefault "localhost";
+      database.createLocally = true;
+      configureRedis = true;
+      datadir = "/nextcloud";
+      config =
+        {
+          adminuser = "init";
+          dbtype = "mysql";
+        }
+        // (lib.optionalAttrs hasNcSecret {
+          adminpassFile = config.sops.secrets."nextcloud/admin-pass".path;
+        });
+    };
   };
 }
