dbb28945fe3148d962766e4737a436fa7b5827c7
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-31 07:26:41 +0300

    [servers/navidrome] add feature flag; gate with mkIf servicesProfiles.navidrome.enable

--- modules/servers/navidrome/default.nix
+++ modules/servers/navidrome/default.nix
@@ -1,9 +1,16 @@
-_: {
-  services.navidrome = {
-    enable = true;
-    openFirewall = true;
-    settings = {
-      MusicFolder = "/one/music";
+{ lib, config, ... }:
+let
+  cfg = config.servicesProfiles.navidrome;
+in {
+  options.servicesProfiles.navidrome.enable = lib.mkEnableOption "Navidrome music server profile";
+
+  config = lib.mkIf cfg.enable {
+    services.navidrome = {
+      enable = true;
+      openFirewall = true;
+      settings = {
+        MusicFolder = "/one/music";
+      };
     };
   };
 }
