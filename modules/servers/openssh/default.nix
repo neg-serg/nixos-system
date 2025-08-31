41bf114e62fef296ccbb2abbf3aa54d054d4c3fb
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-31 07:26:52 +0300

    [servers/openssh] add feature flag; gate with mkIf servicesProfiles.openssh.enable

--- modules/servers/openssh/default.nix
+++ modules/servers/openssh/default.nix
@@ -1,11 +1,18 @@
-{
-  services.openssh = {
-    enable = true;
-    settings = {
-      PasswordAuthentication = false;
-      KbdInteractiveAuthentication = false;
-      PermitRootLogin = "no";
+{ lib, config, ... }:
+let
+  cfg = config.servicesProfiles.openssh;
+in {
+  options.servicesProfiles.openssh.enable = lib.mkEnableOption "OpenSSH + mosh profile";
+
+  config = lib.mkIf cfg.enable {
+    services.openssh = {
+      enable = true;
+      settings = {
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+        PermitRootLogin = "no";
+      };
     };
+    programs.mosh.enable = true; # Opens the relevant UDP ports.
   };
-  programs.mosh.enable = true; # Opens the relevant UDP ports.
 }
