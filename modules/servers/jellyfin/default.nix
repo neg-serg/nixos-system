ba252ee19cf2512163a2380cfde15e8200399125
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-31 07:26:02 +0300

    [servers/jellyfin] add feature flag; gate with mkIf servicesProfiles.jellyfin.enable

--- modules/servers/jellyfin/default.nix
+++ modules/servers/jellyfin/default.nix
@@ -1,6 +1,13 @@
-_: {
-  services.jellyfin = {
-    enable = false;
-    openFirewall = false;
+{ lib, config, ... }:
+let
+  cfg = config.servicesProfiles.jellyfin;
+in {
+  options.servicesProfiles.jellyfin.enable = lib.mkEnableOption "Jellyfin media server profile";
+
+  config = lib.mkIf cfg.enable {
+    services.jellyfin = {
+      enable = true;
+      openFirewall = false;
+    };
   };
 }
