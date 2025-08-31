1ba6a17ef6142cb86214c14ef732c91cb669e9ec
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-31 07:25:20 +0300

    [servers/avahi] add feature flag; gate with mkIf servicesProfiles.avahi.enable

--- modules/servers/avahi/default.nix
+++ modules/servers/avahi/default.nix
@@ -1,13 +1,20 @@
-_: {
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true; # ^^ Needed to allow samba to automatically register mDNS records (without the need for an `extraServiceFile`
-    nssmdns6 = true; # Enable mDNS for IPv6
-    openFirewall = true;
-    publish = {
+{ lib, config, ... }:
+let
+  cfg = config.servicesProfiles.avahi;
+in {
+  options.servicesProfiles.avahi.enable = lib.mkEnableOption "Avahi (mDNS) profile";
+
+  config = lib.mkIf cfg.enable {
+    services.avahi = {
       enable = true;
-      userServices = true;
-      workstation = true;
+      nssmdns4 = true; # Needed for mDNS (IPv4)
+      nssmdns6 = true; # Enable mDNS for IPv6
+      openFirewall = true;
+      publish = {
+        enable = true;
+        userServices = true;
+        workstation = true;
+      };
     };
   };
 }
