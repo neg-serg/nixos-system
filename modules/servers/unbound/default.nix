f9ea9fb1592c7ab0ab5a09eda3cc0baa72e3e372
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-31 07:27:43 +0300

    [servers/unbound] add feature flag; gate with mkIf servicesProfiles.unbound.enable

--- modules/servers/unbound/default.nix
+++ modules/servers/unbound/default.nix
@@ -1,13 +1,20 @@
-_: {
-  services.unbound = {
-    enable = true;
-    settings = {
-      server = {
-        interface = ["127.0.0.1"];
-        port = 5353;
-        "do-tcp" = "yes";
-        "do-udp" = "yes";
-        verbosity = 1;
+{ lib, config, ... }:
+let
+  cfg = config.servicesProfiles.unbound;
+in {
+  options.servicesProfiles.unbound.enable = lib.mkEnableOption "Unbound DNS resolver profile";
+
+  config = lib.mkIf cfg.enable {
+    services.unbound = {
+      enable = true;
+      settings = {
+        server = {
+          interface = ["127.0.0.1"];
+          port = 5353;
+          "do-tcp" = "yes";
+          "do-udp" = "yes";
+          verbosity = 1;
+        };
       };
     };
   };
