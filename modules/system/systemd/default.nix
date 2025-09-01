d1df8da41103a6265860f4aa0b56a92396d24791
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-01 23:36:03 +0300

    [system/systemd] Weight user.slice high; lower nix-daemon\n\nSet user.slice CPU/IOWeight=10000; nix-daemon CPU/IOWeight=200.

--- modules/system/systemd/default.nix
+++ modules/system/systemd/default.nix
@@ -23,6 +23,15 @@
         RebootWatchdogSec = "10s";
       };
     };
+    # Favor user responsiveness; de-prioritize nix-daemon slightly
+    slices."user.slice".sliceConfig = {
+      CPUWeight = 10000;
+      IOWeight = 10000;
+    };
+    services.nix-daemon.serviceConfig = {
+      CPUWeight = 200;
+      IOWeight = 200;
+    };
     packages = [pkgs.packagekit];
   };
 }
