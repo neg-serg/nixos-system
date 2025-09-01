46bb58421b6dc538068b4d03dc6e61901005cafc
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-01 11:32:15 +0300

    [system/virt] Gate virtualization stack on !profiles.vm (lighter VM guest)

--- modules/system/virt.nix
+++ modules/system/virt.nix
@@ -1,48 +1,53 @@
-{ pkgs, ... }: {
-  users.users.neg.extraGroups = ["video" "render"];
-  virtualisation = {
-    containers.enable = true;
+{ pkgs, lib, config, ... }:
+let
+  vmEnabled = (config.profiles.vm or { enable = false; }).enable;
+in {
+  config = lib.mkIf (!vmEnabled) {
+    users.users.neg.extraGroups = ["video" "render"];
+    virtualisation = {
+      containers.enable = true;
 
-    podman = {
-      enable = true;
-      dockerCompat = true; # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerSocket.enable = true; # Create docker alias for compatibility
-      defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
-    };
-
-    oci-containers.backend = "podman";
-
-    docker = {
-      enable = false;
-      autoPrune = {
+      podman = {
         enable = true;
-        dates = "weekly";
-        flags = ["--all"];
+        dockerCompat = true; # Create a `docker` alias for podman, to use it as a drop-in replacement
+        dockerSocket.enable = true; # Create docker alias for compatibility
+        defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
       };
-    };
 
-    libvirtd = {
-      enable = true;
-      qemu = {
-        package = pkgs.qemu_kvm;
-        runAsRoot = true;
-        vhostUserPackages = [pkgs.virtiofsd];
-        swtpm.enable = true;
-        ovmf = {
+      oci-containers.backend = "podman";
+
+      docker = {
+        enable = false;
+        autoPrune = {
           enable = true;
-          packages = [
-            (pkgs.OVMF.override {
-              secureBoot = true;
-              tpmSupport = true;
-            }).fd
-          ];
+          dates = "weekly";
+          flags = ["--all"];
+        };
+      };
+
+      libvirtd = {
+        enable = true;
+        qemu = {
+          package = pkgs.qemu_kvm;
+          runAsRoot = true;
+          vhostUserPackages = [pkgs.virtiofsd];
+          swtpm.enable = true;
+          ovmf = {
+            enable = true;
+            packages = [
+              (pkgs.OVMF.override {
+                secureBoot = true;
+                tpmSupport = true;
+              }).fd
+            ];
+          };
         };
       };
     };
+
+    programs.virt-manager.enable = true;
+    services.spice-webdavd.enable = true;
+
+    imports = [ ./virt/pkgs.nix ];
   };
-
-  programs.virt-manager.enable = true;
-  services.spice-webdavd.enable = true;
-
-  imports = [ ./virt/pkgs.nix ];
 }
