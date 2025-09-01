b24f2548fb6491b3ec83209952a0dde170614b90
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-01 09:47:04 +0300

    [kernel] Split module: params.nix, sysctl.nix, patches-amd.nix; wire imports (no behavior change)

---
+++ modules/system/kernel/params.nix
@@ -0,0 +1,90 @@
+{
+  lib,
+  pkgs,
+  config,
+  kexec_enabled,
+  ...
+}: let
+  # Toggles from profiles/performance.nix
+  perfEnabled = config.profiles.performance.enable or false;
+
+  mitigations_settings = ["mitigations=off"]; # full mitigations disable
+  silence = [
+    "quiet"
+    "rd.systemd.show_status=auto"
+    "rd.udev.log_priority=3"
+    "splash"
+    "systemd.show_status=false"
+    "vt.global_cursor_default=0"
+  ];
+  intel_hda_modules = [
+    "snd_hda_codec"
+    "snd_hda_codec_hdmi"
+    "snd_hda_core"
+    "snd_hda_intel"
+  ];
+  extra_security = ["page_alloc.shuffle=1"];
+  idle_nomwait = ["idle=nomwait"]; # latency over power
+  usb_noautosuspend = ["usbcore.autosuspend=-1"]; # avoid hiccups
+  no_watchdog = [
+    "nowatchdog"
+    "kernel.nmi_watchdog=0"
+  ];
+  obscure_network_protocols = ["ax25" "netrom" "rose"];
+  old_rare_insufficiently_audited_fs = [
+    "adfs" "affs" "befs" "bfs" "cramfs" "efs" "erofs" "exofs" "freevxfs" "gfs2" "hfs"
+    "hfsplus" "hpfs" "jffs2" "jfs" "ksmbd" "minix" "nilfs2" "omfs" "qnx4" "qnx6"
+    "squashfs" "sysv" "ufs" "vivid"
+  ];
+
+  base_params = [
+    "net.ifnames=0"
+  ];
+
+  perf_params =
+    lib.optionals config.profiles.performance.disableAudit ["audit=0"]
+    ++ lib.optionals config.profiles.performance.skipCryptoSelftests ["cryptomgr.notests"]
+    ++ lib.optionals config.profiles.performance.noreplaceSmp ["noreplace-smp"]
+    ++ lib.optionals config.profiles.performance.pciePerformance ["pcie_aspm=performance"]
+    ++ lib.optionals config.profiles.performance.lowLatencyScheduling ["preempt=full" "threadirqs"]
+    ++ lib.optionals config.profiles.performance.fastRCU ["rcupdate.rcu_expedited=1"]
+    ++ lib.optionals config.profiles.performance.trustTSC ["tsc=reliable"]
+    ++ lib.optionals config.profiles.performance.disableSplitLockDetect ["split_lock_detect=off"]
+    ++ lib.optionals config.profiles.performance.disableMitigations mitigations_settings
+    ++ lib.optionals config.profiles.performance.quietBoot silence
+    ++ lib.optionals config.profiles.performance.disableWatchdogs no_watchdog
+    ++ lib.optionals config.profiles.performance.idleNoMwait idle_nomwait
+    ++ lib.optionals config.profiles.performance.disableUsbAutosuspend usb_noautosuspend
+    ++ [
+      "amd_pstate=active"
+      "nvme_core.default_ps_max_latency_us=0"
+    ]
+    ++ lib.optionals (config.profiles.performance.zswap.enable or false) [
+      "zswap.enabled=1"
+      "zswap.compressor=${config.profiles.performance.zswap.compressor}"
+      "zswap.max_pool_percent=${builtins.toString config.profiles.performance.zswap.maxPoolPercent}"
+      "zswap.zpool=${config.profiles.performance.zswap.zpool}"
+    ];
+in {
+  boot = {
+    kernelModules = ["kvm-amd" "tcp_bbr" "amdgpu" "ntsync"];
+    blacklistedKernelModules =
+      ["sp5100_tco"]
+      ++ obscure_network_protocols
+      ++ intel_hda_modules
+      ++ old_rare_insufficiently_audited_fs;
+
+    kernelParams =
+      base_params
+      ++ lib.optionals perfEnabled perf_params
+      ++ extra_security
+      ++ lib.optionals (config.profiles.security.enable or false) ["page_poison=1"];
+
+    extraModulePackages = [pkgs.linuxPackages_cachyos.amneziawg];
+    consoleLogLevel = 3;
+    kernelPackages = pkgs.linuxPackages_cachyos.cachyOverride { mArch = "GENERIC_V4"; };
+  };
+
+  security.protectKernelImage = !kexec_enabled;
+}
+
---
+++ modules/system/kernel/patches-amd.nix
@@ -0,0 +1,51 @@
+{
+  lib,
+  ...
+}: let
+  inherit (lib.kernel) yes no freeform;
+  inherit (lib.attrsets) mapAttrs;
+  inherit (lib.modules) mkForce;
+in {
+  boot.kernelPatches = [
+    {
+      name = "amd-platform-patches";
+      patch = null; # no external patch; apply structured config below
+      structuredExtraConfig = mapAttrs (_: mkForce) {
+        X86_AMD_PSTATE = yes;
+        X86_EXTENDED_PLATFORM = no;
+        X86_MCE_INTEL = no;
+        LRU_GEN = yes;
+        LRU_GEN_ENABLED = yes;
+        CPU_FREQ_STAT = yes;
+
+        HZ = freeform "1000";
+        HZ_250 = lib.mkForce no;
+        HZ_1000 = lib.mkForce yes;
+
+        PREEMPT = yes;
+        PREEMPT_BUILD = yes;
+        PREEMPT_COUNT = yes;
+        PREEMPT_VOLUNTARY = no;
+        PREEMPTION = yes;
+
+        TREE_RCU = yes;
+        PREEMPT_RCU = yes;
+        RCU_EXPERT = yes;
+        TREE_SRCU = yes;
+        TASKS_RCU_GENERIC = yes;
+        TASKS_RCU = yes;
+        TASKS_RUDE_RCU = yes;
+        TASKS_TRACE_RCU = yes;
+        RCU_STALL_COMMON = yes;
+        RCU_NEED_SEGCBLIST = yes;
+        RCU_FANOUT = freeform "64";
+        RCU_FANOUT_LEAF = freeform "16";
+        RCU_BOOST = yes;
+        RCU_BOOST_DELAY = freeform "500";
+        RCU_NOCB_CPU = yes;
+        RCU_LAZY = yes;
+      };
+    }
+  ];
+}
+
