98604bc356ede48be854c20f1c57dc39c5417b09
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-02 21:01:47 +0300

    [security/firejail] Add wrapped binaries for common apps
    Wrap zathura, discord, telegram-desktop, qutebrowser, thunar using upstream profiles.

--- modules/security/firejail.nix
+++ modules/security/firejail.nix
@@ -10,6 +10,26 @@
         executable = "${lib.getBin pkgs.mpv}/bin/mpv";
         profile = "${pkgs.firejail}/etc/firejail/mpv.profile";
       };
+      zathura = {
+        executable = "${lib.getBin pkgs.zathura}/bin/zathura";
+        profile = "${pkgs.firejail}/etc/firejail/zathura.profile";
+      };
+      discord = {
+        executable = "${lib.getBin pkgs.discord}/bin/discord";
+        profile = "${pkgs.firejail}/etc/firejail/discord.profile";
+      };
+      telegram-desktop = {
+        executable = "${lib.getBin pkgs.tdesktop}/bin/telegram-desktop";
+        profile = "${pkgs.firejail}/etc/firejail/telegram-desktop.profile";
+      };
+      qutebrowser = {
+        executable = "${lib.getBin pkgs.qutebrowser}/bin/qutebrowser";
+        profile = "${pkgs.firejail}/etc/firejail/qutebrowser.profile";
+      };
+      thunar = {
+        executable = "${lib.getBin pkgs.xfce.thunar}/bin/thunar";
+        profile = "${pkgs.firejail}/etc/firejail/thunar.profile";
+      };
     };
   };
 }
