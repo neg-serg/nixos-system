8d82a47e9b0a15a750022718ccf211c25983f0f8
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-07 15:48:17 +0300

    [pkgs] its perf now

--- modules/monitoring/pkgs/default.nix
+++ modules/monitoring/pkgs/default.nix
@@ -7,9 +7,8 @@
     btop # even more fancy top
     dool # example use: dool -cdnpmgs --top-bio --top-cpu --top-mem (dstat is not supported as standalone tool anymore)
     iotop # top for iops
-    iperf
-    iperf2 # IP bandwidth measurement
-    linuxPackages_latest.perf
+    iperf iperf2 # IP bandwidth measurement
+    perf # linux profile tools
     linuxPackages_latest.turbostat # cpu monitor
     nethogs # network traffic per process
     powertop # watch for power events
