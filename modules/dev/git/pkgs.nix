ab8d2b94f40c72e1f43fe65536fb14a7989cf166
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-13 12:37:57 +0300

    [jujutsu] add

--- modules/dev/git/pkgs.nix
+++ modules/dev/git/pkgs.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
+    jujutsu # jj: a Git-compatible VCS
     git-crypt # git-based encryption
     git-extras # git extra commands
     git-filter-repo # quickly rewrite git history
