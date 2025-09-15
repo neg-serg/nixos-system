ae59a4c2921d35d10f51fb105b5f09af02f04d63
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-09-16 00:38:56 +0300

    [cleanup] Remove unused lambda args in modules/cli/pkgs.nix

--- modules/cli/pkgs.nix
+++ modules/cli/pkgs.nix
@@ -1,12 +1,13 @@
-{ pkgs, lib, ... }:
-let
+{pkgs, ...}: let
   # Wrap ugrep/ug to always load the system-wide /etc/ugrep.conf
   ugrepWithConfig = pkgs.ugrep.overrideAttrs (old: {
-    nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkgs.makeWrapper ];
-    postInstall = (old.postInstall or "") + ''
-      wrapProgram "$out/bin/ugrep" --add-flags "--config=/etc/ugrep.conf"
-      wrapProgram "$out/bin/ug" --add-flags "--config=/etc/ugrep.conf"
-    '';
+    nativeBuildInputs = (old.nativeBuildInputs or []) ++ [pkgs.makeWrapper];
+    postInstall =
+      (old.postInstall or "")
+      + ''
+        wrapProgram "$out/bin/ugrep" --add-flags "--config=/etc/ugrep.conf"
+        wrapProgram "$out/bin/ug" --add-flags "--config=/etc/ugrep.conf"
+      '';
   });
 in {
   environment.systemPackages = with pkgs; [
