9cda4f81777b00d9882374dd9fa6240505d66c16
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-10-11 09:38:50 +0300

    [hw/amd] Stop forcing VDPAU shims

--- modules/hardware/video/pkgs/default.nix
+++ modules/hardware/video/pkgs/default.nix
@@ -4,6 +4,5 @@
     pkgs.ddcutil # rule monitor params
     pkgs.edid-decode # edid decoder and tester
     pkgs.read-edid # tool to read and parse edid from monitors
-    pkgs.xcalib # stuff for icc profiles
   ];
 }
