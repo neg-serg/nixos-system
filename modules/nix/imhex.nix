9b25f528d9b345cc2c906e8a57b83d2a1b3c9ecd
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-10-11 09:38:33 +0300

    [nix] Relax imhex deprecated warnings

--- modules/nix/imhex.nix
+++ modules/nix/imhex.nix
@@ -7,6 +7,10 @@
         patches = (old.patches or []) ++ [
           ./patches/imhex-edlib-min-cmake-3_5.patch
         ];
+        # Make the build treat deprecated declarations as warnings rather than errors.
+        NIX_CFLAGS_COMPILE =
+          ((old.NIX_CFLAGS_COMPILE or "")
+            + " -Wno-error=deprecated-declarations");
       });
     })
   ];
