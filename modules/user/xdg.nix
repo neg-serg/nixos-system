e463b541b7c9f20cad5b0107a054c6cc749e7707
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-10-15 15:58:00 +0300

    [xdg] Remove KDE portal entry and Plasma defaults to keep Hyprland-only

--- modules/user/xdg.nix
+++ modules/user/xdg.nix
@@ -6,15 +6,13 @@
       # Hyprland portal is provided via programs.hyprland.portalPackage
       pkgs.xdg-desktop-portal-termfilechooser
       pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal-kde
     ];
     config = {
       common.default = ["hyprland" "gtk"];
       common."org.freedesktop.impl.portal.FileChooser" = ["gtk"];
       hyprland.default = ["hyprland" "gtk"];
       hyprland."org.freedesktop.impl.portal.FileChooser" = ["gtk"];
-      # Use KDE portal implementations when running Plasma
-      plasma.default = ["kde" "gtk"];
+      # Plasma-specific portal defaults removed to restore Hyprland-only setup
     };
   };
 }
