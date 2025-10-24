9c7c9db81f855360bae18fb137215afb25a963ef
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-10-24 23:05:35 +0300

    [docs/options] Make nixosOptionsDoc available via lib or pkgs.lib

--- flake.nix
+++ flake.nix
@@ -131,9 +131,11 @@
           hardware = [./modules/hardware];
         };
         evals = lib.mapAttrs (_: evalMods) groups;
-        hasOptionsDoc = lib ? nixosOptionsDoc;
+        # Some lib exports expose nixosOptionsDoc under pkgs.lib rather than nixpkgs.lib
+        docLib = if lib ? nixosOptionsDoc then lib else pkgs.lib;
+        hasOptionsDoc = docLib ? nixosOptionsDoc;
         docs = lib.optionalAttrs hasOptionsDoc (
-          lib.mapAttrs (_: eval: lib.nixosOptionsDoc {inherit (eval) options;}) evals
+          lib.mapAttrs (_: eval: docLib.nixosOptionsDoc {inherit (eval) options;}) evals
         );
         get = name: (builtins.getAttr name docs).optionsCommonMark;
         # Map group keys to output slugs and generate per-doc outputs
