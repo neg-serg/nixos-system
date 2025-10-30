02469a60d64bc87825147511e9f4749b1f0d31f4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-10-30 04:27:38 +0300

    [telfir] Switch to systemd-boot; disable lanzaboote
    Add lib arg; force-disable boot.lanzaboote and enable systemd-boot to work around upstream Rust packaging issue in lanzaboote.

--- hosts/telfir/extra.nix
+++ hosts/telfir/extra.nix
@@ -1,4 +1,9 @@
-{...}: {
+{ lib, ... }: {
   imports = [../../modules/diff-closures.nix];
   diffClosures.enable = true;
+
+  # Workaround Rust packaging issue in lanzaboote by disabling it on this host
+  # and using systemd-boot instead. Revert once upstream is fixed.
+  boot.lanzaboote.enable = lib.mkForce false;
+  boot.loader.systemd-boot.enable = true;
 }
