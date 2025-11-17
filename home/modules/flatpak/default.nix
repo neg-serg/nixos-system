3f31e9a60d31d14a685b84ca79746c5e34a55ce2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-11-17 12:19:43 +0300

    [hm/flatpak] Add Flatpak integration

---
+++ home/modules/flatpak/default.nix
@@ -0,0 +1,5 @@
+_: {
+  home.sessionVariables = {
+    XDG_DATA_DIRS = "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share"; # lets flatpak work
+  };
+}
