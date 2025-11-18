e2643a5582dbafc6e6f6fa91c71a8b570e539755
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-11-18 22:30:25 +0300

    [zsh] fix zshenv

--- modules/user/session/pkgs.nix
+++ modules/user/session/pkgs.nix
@@ -11,10 +11,18 @@ in {
       inputs.raise.defaultPackage.${pkgs.stdenv.hostPlatform.system} # run-or-raise for Hyprland
       pkgs.xorg.xeyes # track eyes for your cursor
       pkgs.swaybg # simple wallpaper setter
+      pkgs.dragon-drop # drag-n-drop from console
+      pkgs.gowall # generate palette from wallpaper
       pkgs.grimblast # Hyprland-friendly screenshots (grim+slurp+wl-copy)
       pkgs.grim # raw screenshot helper for clip wrappers
       pkgs.slurp # select regions for grim/wlroots compositors
+      pkgs.swww # Wayland wallpaper daemon
+      pkgs.waybar # Wayland status bar
+      pkgs.waypipe # Wayland remoting (ssh -X like)
+      pkgs.wev # xev for Wayland
+      pkgs.wf-recorder # screen recording
       pkgs.wl-clipboard # wl-copy / wl-paste
+      pkgs.wl-clip-persist # persist clipboard across app exits
       pkgs.cliphist # persistent Wayland clipboard history
       pkgs.wtype # fake typing for Wayland automation
       pkgs.ydotool # uinput automation helper (autoclicker, etc.)
@@ -26,5 +34,6 @@ in {
       pkgs.hyprpicker # color picker for Wayland/Hyprland
       pkgs.hyprlandPlugins.hy3
       pkgs.hyprlandPlugins.hyprsplit
-    ];
+    ]
+    ++ lib.optionals (pkgs ? uwsm) [pkgs.uwsm];
 }
