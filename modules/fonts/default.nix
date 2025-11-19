7b0a912dff83abdd790024f4ec839cf93aa53365
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-11-20 00:01:44 +0300

    [fonts] Install Iosevka system-wide

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -1,7 +1,23 @@
-{lib, config, pkgs, ...}: let
+{
+  lib,
+  config,
+  pkgs,
+  inputs ? {},
+  ...
+}: let
   guiEnabled = config.features.gui.enable or false;
+  system = pkgs.stdenv.hostPlatform.system;
+  iosevkaInput =
+    if inputs ? "iosevka-neg"
+    then inputs."iosevka-neg".packages.${system}
+    else null;
+  iosevkaFont =
+    if iosevkaInput != null && (iosevkaInput ? nerd-font)
+    then iosevkaInput.nerd-font
+    else pkgs.nerd-fonts.iosevka;
   packages = [
     pkgs.pango
+    iosevkaFont
   ];
 in {
   config = lib.mkIf guiEnabled {
