36b5699b163bcecb1be4c1444f3956fb5ff21982
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-11-22 15:55:08 +0300

    [gui/hyprland] Drop portal module

--- home/modules/user/gui/hyprland/portal.nix
+++
@@ -1,9 +0,0 @@
-{
-  lib,
-  ...
-}: {
-  # Ensure xdg-desktop-portal-hyprland is not skipped due to ConditionEnvironment.
-  # Hyprland already imports WAYLAND_DISPLAY into the systemd user manager.
-  systemd.user.services.xdg-desktop-portal-hyprland.Unit.ConditionEnvironment = lib.mkForce "";
-}
-
