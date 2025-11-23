726cbf6f1f0814333671b50425e752de86936b70
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-11-23 14:13:59 +0300

    [dev/git] Add git-annex

--- modules/dev/git/pkgs.nix
+++ modules/dev/git/pkgs.nix
@@ -5,6 +5,7 @@
     pkgs.git-extras # git extra commands
     pkgs.git-filter-repo # quickly rewrite git history
     pkgs.git-lfs # git extension for large files
+    pkgs.git-annex # manage large/partial content with git
     pkgs.git # my favorite DVCS
     pkgs.act # run GitHub Actions locally
     pkgs.gh # GitHub CLI
