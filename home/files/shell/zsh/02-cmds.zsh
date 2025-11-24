71efb971da743e641b530f77993d35e8b6246e7f
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-11-24 03:44:22 +0300

    [shell/zsh] Fix journalctl alias block

--- home/files/shell/zsh/02-cmds.zsh
+++ home/files/shell/zsh/02-cmds.zsh
@@ -27,10 +27,8 @@ _exists sudo && {
     local sudo_list=(chmod chown modprobe umount)
     local logind_sudo_list=(reboot halt poweroff)
     for c in ${sudo_list[@]}; {_exists "$c" && alias "$c=sudo $c"}
-    }
-    _exists journalctl && journalctl() {command journalctl "${@:--b}";}
-_exists mpv && {
 }
+_exists journalctl && journalctl() {command journalctl "${@:--b}";}
 _exists mpc && {
     cdm(){
         dirname="$XDG_MUSIC_DIR/$(dirname "$(mpc -f '%file%'|head -1)")"
