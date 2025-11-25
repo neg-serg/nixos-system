13adfe658d5cae0b7390f4f682492217925a4fad
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-11-25 03:32:51 +0300

    [shell/zsh] Trim df alias

--- home/files/shell/zsh/06-aliae-fallback.zsh
+++ home/files/shell/zsh/06-aliae-fallback.zsh
@@ -185,7 +185,7 @@ if command -v prettyping >/dev/null 2>&1; then
 fi
 
 if command -v duf >/dev/null 2>&1; then
-  alias df='duf -theme ansi -hide special -hide-mp $HOME/* /nix/store /var/lib/*'
+alias df='duf -theme ansi -hide special -hide-mp $HOME/* /nix/store'
 fi
 
 if command -v dust >/dev/null 2>&1; then
