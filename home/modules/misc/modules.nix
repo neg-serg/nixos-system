2d00f0dcfdec1c8eccf48c3abb2ad14e6f0c022d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-11-27 23:03:12 +0300

    [home/misc] Stop importing bare directories

--- home/modules/misc/modules.nix
+++ home/modules/misc/modules.nix
@@ -1,16 +1,11 @@
 {
   imports = [
-    ./doh
     ./dosemu.nix
-    ./fun-art
     ./fun-art.nix
-    ./rustmission
     ./rustmission.nix
-    ./transmission-daemon
     ./unfree-auto.nix
     ./unfree-libretro.nix
     ./unfree.nix
     ./winboat.nix
-    ./zapret
   ];
 }
