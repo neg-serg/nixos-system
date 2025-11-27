c7dc1d3b9ecbaa67621d636d8f1d37e58ecbed96
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-11-27 22:40:46 +0300

    [db] Add modules.nix aggregator

--- modules/db/default.nix
+++ modules/db/default.nix
@@ -1,12 +1,1 @@
-{lib, config, pkgs, ...}: let
-  enabled = config.features.dev.enable or false;
-  packages = [
-    pkgs.sqlite # self-contained, serverless SQL DB
-    pkgs.pgcli # PostgreSQL TUI client
-    pkgs.iredis # Redis enhanced CLI
-  ];
-in {
-  config = lib.mkIf enabled {
-    environment.systemPackages = lib.mkAfter packages;
-  };
-}
+{...}: import ./modules.nix
