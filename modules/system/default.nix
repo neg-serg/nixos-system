67e69e062ed7707d03ee4176ebee1ee833ec5186
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-11-27 22:30:28 +0300

    [system] Add modules.nix aggregator

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -1,30 +1,6 @@
 {inputs, ...}: {
   imports = [
-    ./boot.nix
-    ./environment.nix
-    ./filesystems.nix
-    ./swapfile.nix
-    ./kernel/params.nix
-    ./kernel/sysctl.nix
-    ./kernel/sysctl-writeback.nix
-    ./kernel/sysctl-mem-extras.nix
-    ./kernel/sysctl-net-extras.nix
-    ./kernel/patches-amd.nix
-    ./profiles/security.nix
-    ./profiles/performance.nix
-    ./profiles/debug.nix
+    ./modules.nix
     (inputs.self + "/modules/hardware/uinput.nix")
-    ./profiles/work.nix
-    ./profiles/vm.nix
-    ./profiles/aliases.nix
-    ./net
-    ./oomd.nix
-    ./irqbalance.nix
-    ./zram.nix
-    ./pkgs.nix
-    ./systemd
-    ./users.nix
-    ./virt.nix
-    ./winapps.nix
   ];
 }
---
+++ modules/system/modules.nix
@@ -0,0 +1,15 @@
+{
+  imports = [
+    ./systemd
+    ./net
+    ./profiles
+    ./kernel
+    ./boot
+    ./virt
+    ./oomd.nix
+    ./nix-ld.nix
+    ./pkgs.nix
+    ./random.nix
+    ./time.nix
+  ];
+}
